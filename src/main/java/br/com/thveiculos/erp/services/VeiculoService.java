b4a91872310404465925f80b6a3b82ad00a29e57
Victor Vilar <victor.svilar@gmail.com>
2025-03-17 15:33:26 -0300

    feat:Criada interface VeiculoService e classe VeiculoServiceImplementation.

---
+++ src/main/java/br/com/thveiculos/erp/services/VeiculoService.java
@@ -0,0 +1,5 @@
+package br.com.thveiculos.erp.services;
+
+public interface VeiculoService {
+
+}
---
+++ src/main/java/br/com/thveiculos/erp/services/VeiculoServiceImplementation.java
@@ -0,0 +1,5 @@
+package br.com.thveiculos.erp.services;
+
+public class VeiculoServiceImplementation {
+
+}
