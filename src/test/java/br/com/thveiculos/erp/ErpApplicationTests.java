3388152c7c485f31fced1b856aa946042598b291
Victor Vilar <victor.svilar@gmail.com>
2025-03-17 14:00:57 -0300

    style:first commit.

---
+++ src/test/java/br/com/thveiculos/erp/ErpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thveiculos.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
