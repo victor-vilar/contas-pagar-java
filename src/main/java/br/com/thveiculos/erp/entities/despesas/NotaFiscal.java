290748176edd5f57775dbcd8a2affea7b83ab7b6
Victor Vilar <victor.svilar@gmail.com>
2025-03-18 15:41:10 -0300

    feat:Implementada classe NotaFiscal.

--- src/main/java/br/com/thveiculos/erp/entities/despesas/NotaFiscal.java
+++ src/main/java/br/com/thveiculos/erp/entities/despesas/NotaFiscal.java
@@ -1,5 +1,50 @@
 package br.com.thveiculos.erp.entities.despesas;
 
-public class NotaFiscal {
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="notas_ficais")
+public class NotaFiscal implements Serializable {
+
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String numero;
+	private LocalDate dataEmissao;
+	
+	public NotaFiscal() {
+		
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public LocalDate getDataEmissao() {
+		return dataEmissao;
+	}
+	public void setDataEmissao(LocalDate dataEmissao) {
+		this.dataEmissao = dataEmissao;
+	}
+	
+	
+	
 
 }
