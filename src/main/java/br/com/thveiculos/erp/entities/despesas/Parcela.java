eadbf9ddade9004034b0addf66cd8e6e8a4feecf
Victor Vilar <victor.svilar@gmail.com>
2025-03-18 15:45:56 -0300

    feat:Implementada classe Parcela.

--- src/main/java/br/com/thveiculos/erp/entities/despesas/Parcela.java
+++ src/main/java/br/com/thveiculos/erp/entities/despesas/Parcela.java
@@ -1,5 +1,74 @@
 package br.com.thveiculos.erp.entities.despesas;
 
-public class Parcela {
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="despesas_parcelas")
+public class Parcela implements Serializable {
+
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String numero;
+	private LocalDate dataPagamento;
+	private BigDecimal valorPagamento;
+	
+	
+	public Parcela () {
+		
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNumero() {
+		return numero;
+	}
+
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+
+	public LocalDate getDataPagamento() {
+		return dataPagamento;
+	}
+
+
+	public void setDataPagamento(LocalDate dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
+
+	public BigDecimal getValorPagamento() {
+		return valorPagamento;
+	}
+
+
+	public void setValorPagamento(BigDecimal valorPagamento) {
+		this.valorPagamento = valorPagamento;
+	}
+	
+	
+	
 
 }
