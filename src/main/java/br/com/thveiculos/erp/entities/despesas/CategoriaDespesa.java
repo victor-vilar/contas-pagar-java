9e4ebef8bc174cf205254c5045f6ded2ea70bd0a
Victor Vilar <victor.svilar@gmail.com>
2025-03-18 15:17:43 -0300

    feat:Implemtada classe CategoriaPagamento.

--- src/main/java/br/com/thveiculos/erp/entities/despesas/CategoriaDespesa.java
+++ src/main/java/br/com/thveiculos/erp/entities/despesas/CategoriaDespesa.java
@@ -1,5 +1,43 @@
 package br.com.thveiculos.erp.entities.despesas;
 
-public enum CategoriaDespesa {
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="categorias_despesas")
+public class CategoriaDespesa implements Serializable {
+
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String categoria;
+	
+	public CategoriaDespesa() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	
 
 }
--- src/main/java/br/com/thveiculos/erp/entities/despesas/FormaPagamento.java
+++ src/main/java/br/com/thveiculos/erp/entities/despesas/FormaPagamento.java
@@ -1,15 +1,38 @@
 package br.com.thveiculos.erp.entities.despesas;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class FormaPagamento {
+public class FormaPagamento implements Serializable {
 
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String forma;
+	
+	public FormaPagamento(){}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getForma() {
+		return forma;
+	}
+
+	public void setForma(String forma) {
+		this.forma = forma;
+	}
+	
+	
 }
