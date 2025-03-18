fd5c976c29d4de4029ff4ccd1b9a568a5f2eeeed
Victor Vilar <victor.svilar@gmail.com>
2025-03-18 15:15:38 -0300

    feat:Adicionanda anotações do JPA e getters e setters da FormaPagamento.

--- src/main/java/br/com/thveiculos/erp/entities/despesas/FormaPagamento.java
+++ src/main/java/br/com/thveiculos/erp/entities/despesas/FormaPagamento.java
@@ -1,5 +1,15 @@
 package br.com.thveiculos.erp.entities.despesas;
 
-public enum FormaPagamento {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class FormaPagamento {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String forma;
 }
