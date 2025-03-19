a2ed7f5bc58297b4bf7e03188dcf4faa3379ceac
Victor Vilar <victor.svilar@gmail.com>
2025-03-19 13:46:57 -0300

    feat:Adicionada dependencia do repository no NotaFiscalServiceImpl.

--- src/main/java/br/com/thveiculos/erp/services/despesas/implementation/NotaFiscalServiceImpl.java
+++ src/main/java/br/com/thveiculos/erp/services/despesas/implementation/NotaFiscalServiceImpl.java
@@ -2,14 +2,23 @@ package br.com.thveiculos.erp.services.despesas.implementation;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.thveiculos.erp.entities.despesas.NotaFiscal;
+import br.com.thveiculos.erp.repositories.despesas.NotaFiscalRepository;
 import br.com.thveiculos.erp.services.despesas.interfaces.NotaFiscalService;
 
 @Service
 public class NotaFiscalServiceImpl implements NotaFiscalService{
 
+	private NotaFiscalRepository repository;
+	
+	@Autowired
+	public NotaFiscalServiceImpl(NotaFiscalRepository repository) {
+		this.repository = repository;
+	}
+	
 	@Override
 	public List<NotaFiscal> getTodos() {
 		// TODO Auto-generated method stub
