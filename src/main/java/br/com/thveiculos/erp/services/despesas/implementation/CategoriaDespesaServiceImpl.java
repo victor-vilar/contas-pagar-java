779b94cb1038d3f537f4e034154535dd74f1c67a
Victor Vilar <victor.svilar@gmail.com>
2025-03-19 13:41:39 -0300

    feat:Adicionada dependencia do repository no CategoriaDespesaServiceImpl.

--- src/main/java/br/com/thveiculos/erp/services/despesas/implementation/CategoriaDespesaServiceImpl.java
+++ src/main/java/br/com/thveiculos/erp/services/despesas/implementation/CategoriaDespesaServiceImpl.java
@@ -2,14 +2,24 @@ package br.com.thveiculos.erp.services.despesas.implementation;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.thveiculos.erp.entities.despesas.CategoriaDespesa;
+import br.com.thveiculos.erp.repositories.despesas.CategoriaDespesaRepository;
 import br.com.thveiculos.erp.services.despesas.interfaces.CategoriaDespesaService;
 
 @Service
 public class CategoriaDespesaServiceImpl implements CategoriaDespesaService {
 
+	
+	private CategoriaDespesaRepository repository;
+	
+	@Autowired
+	public CategoriaDespesaServiceImpl (CategoriaDespesaRepository repositoy) {
+		this.repository = repository; 
+	}
+	
 	@Override
 	public List<CategoriaDespesa> getTodos() {
 		// TODO Auto-generated method stub
