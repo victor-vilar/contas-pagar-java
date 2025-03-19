69c8704c18e825092cdc148e22d89e26f3f1320f
Victor Vilar <victor.svilar@gmail.com>
2025-03-19 13:52:24 -0300

    feat:Adicionada dependencia do repository no ParcelaServiceImpl.

--- src/main/java/br/com/thveiculos/erp/services/despesas/implementation/ParcelaServiceImpl.java
+++ src/main/java/br/com/thveiculos/erp/services/despesas/implementation/ParcelaServiceImpl.java
@@ -2,14 +2,23 @@ package br.com.thveiculos.erp.services.despesas.implementation;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.thveiculos.erp.entities.despesas.Parcela;
+import br.com.thveiculos.erp.repositories.despesas.ParcelaRepository;
 import br.com.thveiculos.erp.services.despesas.interfaces.ParcelaService;
 
 @Service
 public class ParcelaServiceImpl implements ParcelaService{
 
+	private ParcelaRepository repository;
+	
+	@Autowired
+	public ParcelaServiceImpl(ParcelaRepository repository) {
+		this.repository = repository;
+	}
+	
 	@Override
 	public List<Parcela> getTodos() {
 		// TODO Auto-generated method stub
