b0c3b250ee91b54652ddcd7596233c11fbcdc1ec
Victor Vilar <victor.svilar@gmail.com>
2025-03-20 15:47:30 -0300

    feat:Alterado classe principal para não da erro de headless na abertura de um formulário.

--- src/main/java/br/com/thveiculos/erp/ErpApplication.java
+++ src/main/java/br/com/thveiculos/erp/ErpApplication.java
@@ -1,13 +1,23 @@
 package br.com.thveiculos.erp;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "br.com.thveiculos.erp")
 public class ErpApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ErpApplication.class, args);
+		new SpringApplicationBuilder(ErpApplication.class)
+                .web(WebApplicationType.NONE)
+                .headless(false)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
+		
+		//SpringApplication.run(ErpApplication.class, args);
 	}
 
 }
