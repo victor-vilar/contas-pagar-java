3a9cde7ad375b58f5e367ccd9f60731f4590810b
Victor Vilar <victor.svilar@gmail.com>
2025-03-20 17:46:45 -0300

    fix:Removido anotação desnecessária.

--- src/main/java/br/com/thveiculos/erp/ErpApplication.java
+++ src/main/java/br/com/thveiculos/erp/ErpApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication(scanBasePackages = "br.com.thveiculos.erp")
+@SpringBootApplication
 public class ErpApplication {
 
 	public static void main(String[] args) {
