2895bc928e15dd26b98d56e2990103b7a9e60dea
Victor Vilar <victor.svilar@gmail.com>
2025-03-20 14:18:20 -0300

    Criado pacote para armazenar as classes controladoras da view e criada pacote para separa-las.

---
+++ src/main/java/br/com/thveiculos/erp/controllers/despesas/FormaPagamentoController.java
@@ -0,0 +1,44 @@
+package br.com.thveiculos.erp.controllers.despesas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.DefaultTableModel;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.com.thveiculos.erp.entities.despesas.FormaPagamento;
+import br.com.thveiculos.erp.services.despesas.interfaces.FormaPagamentoService;
+import br.com.thveiculos.erp.views.despesas.FormaPagamentoView;
+
+@Component
+public class FormaPagamentoController {
+
+	private FormaPagamentoView view;
+//	private FormaPagamentoService service;
+//	
+//	@Autowired
+//	public FormaPagamentoController(FormaPagamentoService service) {
+//		this.service = service;
+//	}
+	
+	
+	public void setView(FormaPagamentoView view) {
+		this.view = view;
+	}
+	
+	public void salvar() {
+		FormaPagamento formaPagamento = new FormaPagamento();
+		formaPagamento.setForma(view.getFieldFormaPagamento().getText());
+		//service.save(formaPagamento);
+		atualizarTabela();
+	}
+	
+	public void atualizarTabela() {
+		DefaultTableModel model = (DefaultTableModel) view.getTable().getModel();
+		model.addRow(new Object[] {"",""});
+	}
+
+		
+}
