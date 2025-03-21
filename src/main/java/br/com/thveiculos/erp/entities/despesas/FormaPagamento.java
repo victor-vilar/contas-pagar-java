77947426aea984f56620831f80ab7f9423b3d403
Victor Vilar <victor.svilar@gmail.com>
2025-03-21 12:57:49 -0300

    feat:Adicionado Constraint de não permitir uma forma de pagament com o mesmo nome.

--- src/main/java/br/com/thveiculos/erp/entities/despesas/FormaPagamento.java
+++ src/main/java/br/com/thveiculos/erp/entities/despesas/FormaPagamento.java
@@ -2,6 +2,7 @@ package br.com.thveiculos.erp.entities.despesas;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,8 @@ public class FormaPagamento implements Serializable {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(unique=true, nullable = false)
 	private String forma;
 	
 	public FormaPagamento(){}
