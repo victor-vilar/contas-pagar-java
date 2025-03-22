c34866f8d588ab66ba3dd4b61e417b985be8b80d
Victor Vilar <victor.svilar@gmail.com>
2025-03-22 12:06:32 -0300

    feat:Adicionado contraints nas dependencias de CategoriaDespesa e FormaPagamento para que quando houver uma removação de uma dessas dependencias os campos sejam marcados como null pelo hibernate.

--- src/main/java/br/com/thveiculos/erp/entities/despesas/DespesaAbstrata.java
+++ src/main/java/br/com/thveiculos/erp/entities/despesas/DespesaAbstrata.java
@@ -4,9 +4,11 @@ import java.math.BigDecimal;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
@@ -23,9 +25,11 @@ public abstract class DespesaAbstrata implements Despesa{
 	private BigDecimal valorTotal;
 	
 	@ManyToOne
+	@JoinColumn(name = "categoria_fk", foreignKey = @ForeignKey(name="categoria_fk", foreignKeyDefinition = "ON DELETE SET NULL"))
 	private CategoriaDespesa categoria;
 	
 	@ManyToOne
+	@JoinColumn(name = "forma_pagamento_fk", foreignKey = @ForeignKey(name="forma_pagamento_fk", foreignKeyDefinition = "ON DELETE SET NULL"))
 	private FormaPagamento formaPagamento;
 	
 	public Long getId() {
