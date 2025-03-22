78a23c6278d8ebec2f0cb1eb0e9139a7a42d7452
Victor Vilar <victor.svilar@gmail.com>
2025-03-22 15:17:04 -0300

    style:Organizado codigo da view para separar a configuração de todos os seus componentes.

--- src/main/java/br/com/thveiculos/erp/views/despesas/FormaPagamentoView.java
+++ src/main/java/br/com/thveiculos/erp/views/despesas/FormaPagamentoView.java
@@ -1,9 +1,10 @@
 package br.com.thveiculos.erp.views.despesas;
 
-import java.awt.EventQueue;
+import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -12,56 +13,67 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.JToolBar;
 import javax.swing.ListSelectionModel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
 //import br.com.thveiculos.erp.configuration.ApplicationConfiguration;
 import br.com.thveiculos.erp.controllers.despesas.FormaPagamentoController;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 @Component
 public class FormaPagamentoView extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JScrollPane scrollPane;
 	private JTable table;
 	private JTextField fieldFormaPagamento;
+	private JTextField fieldId;
+	private JToolBar toolBar;
 	private JButton btnSalvar;
+	private JButton btnNovo;
+	private JButton btnDeletar;
 	private FormaPagamentoController controller;
 	
-	public JTable getTable() {
-		return this.table;
+	
+	public FormaPagamentoView(FormaPagamentoController controller) {
+		
+		this.controller = controller;
+		controller.setView(this);
+		this.setUp();
 	}
+
 	
 	public JTextField getFieldFormaPagamento() {
 		return this.fieldFormaPagamento;
+	}
+	
+	public JTextField getFieldId() {
+		return this.fieldId;
 	}
 	
 	public JButton getBtnSalvar() {
 		return this.btnSalvar;
 	}
 	
-
-	/**
-	 * Create the frame.
-	 */
-
-	public FormaPagamentoView(FormaPagamentoController controller) {
-		this.controller = controller;
-		controller.setView(this);
-		this.setUp();
+	public JTable getTable() {
+		return this.table;
 	}
+
+
+	
 	
 	public void setUp() {
 		
 		setTitle("Formas de Pagamento");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 443, 329);
+		setResizable(false);
 		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -69,46 +81,51 @@ public class FormaPagamentoView extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		//Criando o ScrollPane
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 60, 414, 150);
-		contentPane.add(scrollPane);
+
+		configurarTollBar();
+		configurarScrollPane();
+		configurarTable();
+		configurarLabels();
+		configurarFields();
+		configurarButtons();
+	}
+
+
+	private void configurarTollBar() {
+
+		//TollBar
+		toolBar = new JToolBar();
+		toolBar.setEnabled(false);
+		toolBar.setBounds(0, 0, 448, 36);
+		toolBar.setFloatable( false);
+		contentPane.add(toolBar);
 		
-		//Criando txt_formaPagamento
-		fieldFormaPagamento = new JTextField();
-		fieldFormaPagamento.setBounds(10, 24, 414, 25);
-		contentPane.add(fieldFormaPagamento);
-		fieldFormaPagamento.setColumns(10);
+	}
+	
+	private void configurarScrollPane(){
+		
+
+		//Criando o ScrollPane
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 143, 426, 150);
+		contentPane.add(scrollPane);
+	}
+	
+	private void configurarTable() {
 		
 		//Criando Tabela
 		table = new JTable();
+		table.setFillsViewportHeight(true);
+		table.setColumnSelectionAllowed(true);
 		table.setCellSelectionEnabled(true);
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		scrollPane.setViewportView(table);
-	    
-		//Adicionando método de duplo clique na tabela
-		table.addMouseListener(new MouseAdapter() {
-	          public void mouseClicked(MouseEvent me) {
-	             //Se teve 2 clicks no mouse
-	        	  if (me.getClickCount() == 2) {     // to detect doble click events
-	                //Pega a tabela
-	        		 JTable target = (JTable)me.getSource();
-	                //pega a linha selecionada
-	        		 int row = target.getSelectedRow(); // select a row
-	                //Pega a coluna selecionada
-	        		 int column = target.getSelectedColumn(); // select a column
-	                //Adiciona o valor das coordenadas passadas para o txt_forma_pagamento
-	        		 fieldFormaPagamento.setText((String)table.getValueAt(row,1));
-	               //JOptionPane.showMessageDialog(null, table.getValueAt(row,column)); // get the value of a row and column.
-	             }
-	          }
-	       });
 		
 	    table.setModel(new DefaultTableModel(
 			new Object[][] {
 			},
 			new String[] {
-				"Id", "Forma de Pagamento"
+				"No", "Forma de Pagamento"
 			}
 		) {
 			Class[] columnTypes = new Class[] {
@@ -117,49 +134,111 @@ public class FormaPagamentoView extends JFrame {
 			public Class getColumnClass(int columnIndex) {
 				return columnTypes[columnIndex];
 			}
+			
+			@Override
+			public boolean isCellEditable(int row, int column){    
+	            return false;
+	        }
 		});
-		
-	    //Impede que os dados na tabela sejam editaveis
-	    table.setDefaultEditor(Object.class, null);
 	    
 	    //seta a primeira coluna com o tamanho 27
-	    table.getColumnModel().getColumn(0).setPreferredWidth(27);
+	    table.getColumnModel().getColumn(0).setMaxWidth(30);
 	    
-	    //seta a ultima coluna para pegar toda a tabela
-	    table.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
+		//Adicionando método de duplo clique na tabela
+		table.addMouseListener(new MouseAdapter() {
+	          public void mouseClicked(MouseEvent me) {
+	            
+	      		// to detect doble click events
+	      		if (me.getClickCount() == 2) {     
+	                //Pega a tabela
+	        		 JTable target = (JTable)me.getSource();
+	               
+	        		 //pega a linha selecionada
+	        		 int row = target.getSelectedRow(); // select a row
+	               
+	        		 //Pega a coluna selecionada
+	        		 int column = target.getSelectedColumn(); // select a column
+	               
+	        		 //Adicionando valor do formulário para os campos para edição
+	        		 fieldFormaPagamento.setText((String) table.getValueAt(row,1));
+	                 fieldId.setText(String.valueOf(table.getValueAt(row, 0)));
+	                 fieldFormaPagamento.setEditable(true);
+	                 fieldFormaPagamento.requestFocus();
+	        		 //JOptionPane.showMessageDialog(null, table.getValueAt(row,column)); // get the value of a row and column.
+	        		 target.clearSelection();
+	             }
+
+	          }
+	       });
 		
-	    
-	    //Criando o Label
+	}
+	
+	
+	private void configurarLabels() {
+		
+		//Id
+		JLabel lblNewLabel = new JLabel("No");
+		lblNewLabel.setBounds(360, 60, 60, 17);
+		contentPane.add(lblNewLabel);
+		
+	    //Nome
 		JLabel lbl_nome = new JLabel("Nome");
-		lbl_nome.setBounds(10, 11, 125, 14);
+		lbl_nome.setBounds(10, 92, 125, 14);
 		contentPane.add(lbl_nome);
+	}
+	
+	
+	
+	private void configurarFields() {
 		
+		//Id
+		fieldId = new JTextField();
+		fieldId.setEditable(false);
+		fieldId.setBounds(360, 77, 76, 25);
+		contentPane.add(fieldId);
+		fieldId.setColumns(10);
 		
-		//Criando botao
-		btnSalvar = new JButton("Salvar");
+		//Criando fieldFormaPagamento
+		fieldFormaPagamento = new JTextField();
+		fieldFormaPagamento.setBounds(10, 106, 426, 25);
+		fieldFormaPagamento.setEditable(false);
+		contentPane.add(fieldFormaPagamento);
+		fieldFormaPagamento.setColumns(10);
 		
-		//Adiciona um evento ao clique do botão
-		btnSalvar.addActionListener((e) ->{
-			this.salvar();
+	}
+	private void configurarButtons() {
+		
+		//Botao de Adicionar Novo
+		btnNovo = new JButton("Novo");
+		btnNovo.setIcon(new ImageIcon("/home/victor/Downloads/novo-arquivo(1).png"));
+		btnNovo.setToolTipText("Adicionar novo");
+		toolBar.add(btnNovo);
+		
+		//Adiciona um evento ao clique do botão de novo
+		btnNovo.addActionListener((e) ->{
+			this.controller.novo();
 		});
 		
-		btnSalvar.setBounds(10, 214, 89, 36);
-		contentPane.add(btnSalvar);
+		//Botao de Salvar
+		btnSalvar = new JButton("Salvar");
+		btnSalvar.setIcon(new ImageIcon("/home/victor/Downloads/salve-.png"));
+		toolBar.add(btnSalvar);
+		btnSalvar.setToolTipText("Salvar");
 		
-	}
-
-	
-	private void salvar() {
-		if(!fieldFormaPagamento.getText().equals("")) {
-			this.controller.salvar();
-		}
+		//Adiciona um evento ao clique do botão de salvar
+		btnSalvar.addActionListener((e) ->{
+			controller.salvar();
+		});
 		
-		fieldFormaPagamento.setText(null);		
-	}
-	
-	private void exibirDialog(String msg) {
-		JOptionPane.showInternalMessageDialog(null, msg);
-	}
+		//Botão de Deletar
+		btnDeletar = new JButton("Deletar");
+		btnDeletar.setIcon(new ImageIcon("/home/victor/Downloads/delete.png"));
+		toolBar.add(btnDeletar);
 		
-	
+		btnDeletar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				controller.deletar();
+			}
+		});
+	}
 }
