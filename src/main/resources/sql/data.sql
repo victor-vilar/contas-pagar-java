f50774746d7e3dc9a928ff23cc0a330bebd38ef9
Victor Vilar <victor.svilar@gmail.com>
2025-03-24 18:38:46 -0300

    feat :Adicionado mais alguns insert no data.sql.

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -4,3 +4,9 @@ INSERT INTO formas_pagamento(forma) VALUES('CARTAO DE DEBITO');
 INSERT INTO formas_pagamento(forma) VALUES('BOLETO BANCÁRIO');
 INSERT INTO formas_pagamento(forma) VALUES('CHEQUE');
 INSERT INTO formas_pagamento(forma) VALUES('DINHEIRO');
+
+
+INSERT INTO categorias_despesas(categoria) VALUES('MANUTENÇÃO VEICULAR');
+INSERT INTO categorias_despesas(categoria) VALUES('ENERGIA');
+INSERT INTO categorias_despesas(categoria) VALUES('DESPESAS DIVERSAS');
+INSERT INTO categorias_despesas(categoria) VALUES('FOLHA DE PAGAMENTO');
