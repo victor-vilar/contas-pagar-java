22ab8c89325a6a20f838c82ade26dd5b37ea895f
Victor Vilar <victor.svilar@gmail.com>
2025-03-24 14:02:24 -0300

    feat:Adicionado mais dados ao data.sql.

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,1 +1,6 @@
 INSERT INTO formas_pagamento(forma) VALUES('PIX');
+INSERT INTO formas_pagamento(forma) VALUES('CARTAO DE CREDITO');
+INSERT INTO formas_pagamento(forma) VALUES('CARTAO DE DEBITO');
+INSERT INTO formas_pagamento(forma) VALUES('BOLETO BANCÁRIO');
+INSERT INTO formas_pagamento(forma) VALUES('CHEQUE');
+INSERT INTO formas_pagamento(forma) VALUES('DINHEIRO');
