f41e2e0a615c9c9e04afd1e3d99949659ce912e3
Victor Vilar <victor.svilar@gmail.com>
2025-03-25 13:06:10 -0300

    feat:Removido o metodo para buscar a forma de pagamento da despesa, já que agora quem tem a forma de pagamento é o movimento. Adicionado metodo para buscar a quantidade de parcelas(movimentos) que a despesa possui.

--- src/main/java/br/com/thveiculos/erp/entities/despesas/Despesa.java
+++ src/main/java/br/com/thveiculos/erp/entities/despesas/Despesa.java
@@ -6,9 +6,9 @@ public interface Despesa {
 
 	
 	public CategoriaDespesa getCategoria();
-	public FormaPagamento getFormaPagamento();
 	public String getNomeFornecedor();
 	public String getDescricao();
 	public boolean isQuitado();
 	public BigDecimal getValorTotal();
+	public int getQuantidadeParcelas();
 }
