2680c529ad0466974798206cd9a7d66f93f813f2
Victor Vilar <victor.svilar@gmail.com>
2025-03-27 18:56:18 -0300

    feat:Alterado o InitView para setar o novo look and feel.

--- src/main/java/br/com/thveiculos/erp/configuration/InitView.java
+++ src/main/java/br/com/thveiculos/erp/configuration/InitView.java
@@ -5,21 +5,32 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import br.com.thveiculos.erp.views.util.MainView;
+import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.UIManager;
 
 @Component
-public class InitView  implements CommandLineRunner {
+public class InitView implements CommandLineRunner {
 
-	
-	private MainView view;
-	
-	@Autowired
-	public InitView(MainView view) {
-		this.view = view;
-	}
-	
-	@Override
-	public void run(String... args) throws Exception {
-		view.setVisible(true);
+    private MainView view;
+
+    @Autowired
+    public InitView(MainView view) {
+        this.view = view;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        setLookAndFeel();
+        view.setVisible(true);
+    }
+
+    private void setLookAndFeel() {
+        try {
+                UIManager.setLookAndFeel( new FlatLightLaf() );
+        }catch (Exception ex) {
+                System.out.println(ex);
+                
+        }
     }
 
 }
