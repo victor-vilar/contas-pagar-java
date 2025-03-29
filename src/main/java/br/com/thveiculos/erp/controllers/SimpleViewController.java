b468d60fff94b18caed4d741e2122fa96f9266eb
Victor Vilar <victor.svilar@gmail.com>
2025-03-29 11:53:18 -0300

    feat:Adicionada anotação da interface AppViewController no SimpleViewController.

--- src/main/java/br/com/thveiculos/erp/controllers/SimpleViewController.java
+++ src/main/java/br/com/thveiculos/erp/controllers/SimpleViewController.java
@@ -12,16 +12,16 @@ import br.com.thveiculos.erp.views.despesas.CategoriaDespesaView;
 import br.com.thveiculos.erp.views.despesas.FormaPagamentoView;
 
 /** 
- * Controlador para as views do tipo {@link SimpleView}, esses formulários são objetos que manipulam objetos do tipo {@link SimpleEntity}.As view {@link FormaPagamentoView} e {@link CategoriaDespesaView} são exemplos de formulários que extendem essa classe.
- * As entidades ao qual elas manipulam possuem somente o id e o nome como propriedades então os formulários e suas carecteristicas e layout
- seguem o mesmo padrão. Essa entidades são conhecidas nesse sistema como 
+ * Controlador para as views do tipo {@link SimpleView}, esses formulários são objetos que manipulam objetos do tipo {@link SimpleEntity}.As views {@link FormaPagamentoView} e {@link CategoriaDespesaView}
+ * são exemplos de formulários que extendem essa classe.
+ * As entidades ao qual elas manipulam possuem somente o id e o nome como propriedades então os formulários e suas carecteristicas e layout seguem o mesmo padrão. Essa entidades são conhecidas nesse
+ * sistema como 
  
- Essas views controlam entidades que se comportam como um ENUM, como quero criar a possibilidade do usuário conseguir registrar mais
- entidades desse tipo, foram criados tabelas no banco de dados para que assim elas possam ser registradas e removidas como qualquer outra classe.
+ *Essas views controlam entidades que se comportam como um ENUM, como quero criar a possibilidade do usuário conseguir registrar mais
+ *entidades desse tipo, foram criados tabelas no banco de dados para que assim elas possam ser registradas e removidas como qualquer outra classe.
  
- Toda e qualquer classe que for um controllador de formulários simples devera então extender essa classe e implementar o metodo que construção de objeto.
  * */
-public abstract class SimpleViewController {
+public abstract class SimpleViewController implements AppViewController<SimpleView> {
 
 	protected SimpleView view;
 	protected AppService service;
@@ -30,11 +30,12 @@ public abstract class SimpleViewController {
 	
 	}
 	
+        @Override
 	public void setView(SimpleView view) {
 		this.view = view;
 	}
 	
-	
+	@Override
 	public void novo() {
                 view.getFieldId().setText("");
                 view.getFieldNome().setText("");
@@ -42,6 +43,7 @@ public abstract class SimpleViewController {
 		view.getFieldNome().requestFocus();
 	}
 	
+        @Override
 	public void salvar() {
 		
 		if(view.getFieldNome().getText().equals("")) {
@@ -53,6 +55,7 @@ public abstract class SimpleViewController {
 		updateView();
 	}
 	
+        @Override
 	public void deletar() {
 		
 		if(view.getTable().getSelectedRow() == -1) {
@@ -65,18 +68,18 @@ public abstract class SimpleViewController {
 		
 		updateView();
 	}
+        
+        @Override
+	public void limparCampos() {
+		view.getFieldId().setText(null);
+		view.getFieldNome().setText(null);
+		
+	}
 	
 	public void updateView() {
 		atualizarTabela();
 		limparCampos();
 		view.getFieldNome().setEditable(false);
-	}
-	
-	
-	public void limparCampos() {
-		view.getFieldId().setText(null);
-		view.getFieldNome().setText(null);
-		
 	}
 	
 	public void atualizarTabela() {
