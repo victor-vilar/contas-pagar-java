4f9c98ce491ff0981c4b3f66ef1c87e0257a72e8
Victor Vilar <victor.svilar@gmail.com>
2025-03-31 14:39:55 -0300

    feat:Adicionanda dependencias, adicionado metodo para buscar e preencher as combo box.

--- src/main/java/br/com/thveiculos/erp/controllers/despesas/DespesaViewController.java
+++ src/main/java/br/com/thveiculos/erp/controllers/despesas/DespesaViewController.java
@@ -2,86 +2,144 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package br.com.thveiculos.erp.controllers;
+package br.com.thveiculos.erp.controllers.despesas;
 
+import br.com.thveiculos.erp.controllers.AppViewController;
+import br.com.thveiculos.erp.controllers.util.ControllerHelper;
 import br.com.thveiculos.erp.entities.despesas.Despesa;
+import br.com.thveiculos.erp.entities.despesas.DespesaAvulsa;
+import br.com.thveiculos.erp.entities.despesas.MovimentoPagamento;
+import br.com.thveiculos.erp.services.despesas.interfaces.CategoriaDespesaService;
 import br.com.thveiculos.erp.services.despesas.interfaces.DespesaService;
+import br.com.thveiculos.erp.services.despesas.interfaces.FormaPagamentoService;
 import br.com.thveiculos.erp.views.despesas.DespesaView;
-import java.util.Arrays;
 import java.util.List;
-import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Controller;
 
 /**
  *
  * @author victor
  */
-@Component
-public class DespesaViewController implements AppViewController<DespesaView>{
-    
+@Controller
+public class DespesaViewController implements AppViewController<DespesaView> {
+
     private final DespesaService service;
     private DespesaView view;
-    private final List<String> exludeComponents = List.of("btnNovo","btnEditar","btnSalvar","btnDeletar","fieldId");
-    
+    private final CategoriaDespesaService categoriaDespesaService;
+    private final FormaPagamentoService formaPagamentoService;
+    private final List<String> exludeComponents = List.of("btnNovo", "btnEditar",
+            "btnSalvar", "btnDeletar", "fieldId");
+
     @Autowired
-    public DespesaViewController(DespesaService service){
-        this.service =  service;
-        
+    public DespesaViewController(
+            DespesaService service,
+            CategoriaDespesaService categoriaDespesaService,
+            FormaPagamentoService formaPagamentoService) {
+        this.service = service;
+        this.categoriaDespesaService = categoriaDespesaService;
+        this.formaPagamentoService = formaPagamentoService;
+
     }
-    
+
     @Override
-    public void setView(DespesaView view){
+    public void setView(DespesaView view) {
         this.view = view;
 
     }
-    
-    
+
     @Override
     public void novo() {
         enableDisableComponents(true);
-        
+        limparCampos();
+
     }
-    
+
     @Override
-    public void salvar(){
-        
+    public void salvar() {
+
         //service.save(build());
-        
-       enableDisableComponents(false);
-        
- 
+        enableDisableComponents(false);
+        limparCampos();
+
     }
-    
+
     @Override
-    public void deletar(){
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void editar() {
+        if(view.getFieldId().getText().equals("")){
+            return;
+        }
+        
+        enableDisableComponents(true);
     }
-    
+
+    @Override
+    public void deletar() {
+        service.deleteById(Long.valueOf(view.getFieldId().getText()));
+    }
 
     @Override
     public void limparCampos() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        view.getTextFields().stream().forEach(f -> f.setText(""));
+        view.getSpinnerQuantidadeParcelas().getModel().setValue(0);
     }
-    
-    
-    public Despesa build(){
+
+    public Despesa build() {
+        DespesaAvulsa despesa = new DespesaAvulsa();
+        
+        
         return null;
     }
+
+    public void atualizarCampos() {
+        addValoresComboCategoria();
+        addValoresComboFormaPagamento();
+        addValoresParcelamento();
+
+    }
+
+    public void addValoresComboCategoria() {
     
-    public void atualizarTabela(){}
-    
-    
-    public void enableDisableComponents(boolean con){
+        this.categoriaDespesaService.getTodos().stream().forEach(c ->{ 
+            System.out.println(c.getName());
+            view.getComboCategoria().addItem(c.getName());
+        });
         
-       view.listaDeComponentes().stream().forEach(c -> {
-           if((c.getName()!= null) &&!exludeComponents.contains(c.getName())){
-               c.setEnabled(con);
-           }
-       });
+        view.getComboCategoria().setSelectedIndex(-1);
+    }
+
+    public void addValoresComboFormaPagamento() {
+        
+        this.formaPagamentoService.getTodos().stream().forEach(f ->{ 
+            System.out.println(f.getName());
+            view.getComboFormaPagamento().addItem(f.getName());
+        });
+        
+        view.getComboFormaPagamento().setSelectedIndex(-1);
+    }
+
+    public void addValoresParcelamento() {
+
+    }
+
+    public void enableDisableComponents(boolean con) {
+
+        view.listaDeComponentes().stream().forEach(c -> {
+            if ((c.getName() != null) && !exludeComponents.contains(c.getName())) {
+                c.setEnabled(con);
+            }
+        });
 
     }
     
-    
-    
+    public void atualizarTabela(List<MovimentoPagamento> movimentos){
+        DefaultTableModel model = (DefaultTableModel) view.getTableParcelas().getModel();
+        ControllerHelper.limparTabela(model);
+        movimentos.stream().forEach(m ->{
+            model.addRow(new Object[]{m.getId(),m.getReferenteParcela(),m.getDataVencimento(),m.getValorPagamento(),m.getDataPagamento()});
+        });
+    }
+
 }
