f65df510141cee0c0230421d972560aed95f5926
Victor Vilar <victor.svilar@gmail.com>
2025-04-01 18:44:41 -0300

    feat:Agora o metodo de gerar e atualizar parcelas é de responsabilidade do DespesaService.

--- src/main/java/br/com/thveiculos/erp/services/despesas/implementation/DespesaServiceImpl.java
+++ src/main/java/br/com/thveiculos/erp/services/despesas/implementation/DespesaServiceImpl.java
@@ -6,59 +6,76 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.thveiculos.erp.entities.despesas.Despesa;
+import br.com.thveiculos.erp.entities.despesas.FormaPagamento;
+import br.com.thveiculos.erp.entities.despesas.MovimentoPagamento;
 import br.com.thveiculos.erp.repositories.despesas.DespesaRepository;
 import br.com.thveiculos.erp.services.despesas.interfaces.DespesaService;
+import java.util.Set;
+import javax.swing.table.DefaultTableModel;
 
 @Service
-public class DespesaServiceImpl implements DespesaService{
+public class DespesaServiceImpl implements DespesaService {
 
-	private DespesaRepository repository;
-	
-	@Autowired
-	public DespesaServiceImpl(DespesaRepository repository) {
-		this.repository = repository;
-	}
-	
-	@Override
-	public List<Despesa> getTodos() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private DespesaRepository repository;
 
-	@Override
-	public Despesa getById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Autowired
+    public DespesaServiceImpl(DespesaRepository repository) {
+        this.repository = repository;
+    }
 
-	@Override
-	public Despesa save(Despesa obj) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<Despesa> getTodos() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public Despesa saveAll(List<Despesa> objs) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Despesa getById(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void deleteById(Long id) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public Despesa save(Despesa obj) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void deleteAll(List<Despesa> objs) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public Despesa saveAll(List<Despesa> objs) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public Despesa update(Despesa obj) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void deleteById(Long id) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void deleteAll(List<Despesa> objs) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Despesa update(Despesa obj) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<MovimentoPagamento> gerarMovimentos(String parcelamento, int qtdParcelas, String dataInicial, String valor, FormaPagamento formaPagamento) {
+        GeradorMovimentos gm = new GeradorMovimentos();
+        return gm.gerarMovimentos(parcelamento, qtdParcelas, dataInicial, valor, formaPagamento);
+
+    }
+
+    @Override
+    public void atualizarMovimentos(List<MovimentoPagamento> movimentos, Set<Integer> linhas, DefaultTableModel model) {
+        GeradorMovimentos gm = new GeradorMovimentos();
+        gm.atualizarMovimentos(movimentos, linhas, model);
+    }
 
 }
--- src/main/java/br/com/thveiculos/erp/services/despesas/interfaces/DespesaService.java
+++ src/main/java/br/com/thveiculos/erp/services/despesas/interfaces/DespesaService.java
@@ -1,7 +1,14 @@
 package br.com.thveiculos.erp.services.despesas.interfaces;
 
 import br.com.thveiculos.erp.entities.despesas.Despesa;
+import br.com.thveiculos.erp.entities.despesas.FormaPagamento;
+import br.com.thveiculos.erp.entities.despesas.MovimentoPagamento;
+import java.util.List;
+import java.util.Set;
+import javax.swing.table.DefaultTableModel;
 
 public interface DespesaService extends AppService<Despesa>{
 
+       public List<MovimentoPagamento> gerarMovimentos(String parcelamento,int qtdParcelas, String dataInicial, String valor, FormaPagamento formaPagamento);
+       public void atualizarMovimentos(List<MovimentoPagamento> movimentos, Set<Integer> linhas, DefaultTableModel model);
 }
