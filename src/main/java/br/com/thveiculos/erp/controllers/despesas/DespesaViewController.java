9d0ba47933fce1f744cf4326ba463fd6a90e3fbc
Victor Vilar <victor.svilar@gmail.com>
2025-04-01 14:57:41 -0300

    feat:Criada duas variaveis contants para informar qual é o tipo de despesa que deverá ser criado, 'DespesaAvulsa' ou 'DespesaRecorrente' - Criada um Hash de linhas selecionadas na view, para conseguir saber quais dessas linhas sofreram alteração para que essas alterações possam ser feitas na lista de movimentos - Criada lista de Movimentos chamada movimentosSnapShot, para eu conseguir comparar os movimentos criados com as alterações realizadas na tabela - Criado metodo para adiconar as linhas alteradas na tabela na lista de linhas selecionadas.

--- src/main/java/br/com/thveiculos/erp/controllers/despesas/DespesaViewController.java
+++ src/main/java/br/com/thveiculos/erp/controllers/despesas/DespesaViewController.java
@@ -8,7 +8,7 @@ import br.com.thveiculos.erp.controllers.AppViewController;
 import br.com.thveiculos.erp.controllers.util.ControllerHelper;
 import br.com.thveiculos.erp.entities.despesas.Despesa;
 import br.com.thveiculos.erp.entities.despesas.DespesaAvulsa;
-import br.com.thveiculos.erp.entities.despesas.FormaPagamento;
+import br.com.thveiculos.erp.entities.despesas.DespesaRecorrente;
 import br.com.thveiculos.erp.entities.despesas.MovimentoPagamento;
 import br.com.thveiculos.erp.enums.despesas.Periodo;
 import br.com.thveiculos.erp.services.despesas.implementation.GeradorMovimentos;
@@ -18,9 +18,12 @@ import br.com.thveiculos.erp.services.despesas.interfaces.FormaPagamentoService;
 import br.com.thveiculos.erp.views.despesas.DespesaView;
 import java.text.NumberFormat;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 import javax.swing.table.DefaultTableModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -36,9 +39,17 @@ public class DespesaViewController implements AppViewController<DespesaView> {
     private DespesaView view;
     private final CategoriaDespesaService categoriaDespesaService;
     private final FormaPagamentoService formaPagamentoService;
+    private static final String DESPESA_AVULA ="AVULSA";
+    private static final String DESPESA_RECORRENTE ="RECORRENTE";
     private final List<String> exludeComponents = List.of("btnNovo", "btnEditar",
             "btnSalvar", "btnDeletar", "fieldId");
 
+   
+    private Set<Integer> linhasSelecionadas;
+    private List<MovimentoPagamento> movimentosSnapShot;
+    
+    
+    
     @Autowired
     public DespesaViewController(
             DespesaService service,
@@ -47,6 +58,8 @@ public class DespesaViewController implements AppViewController<DespesaView> {
         this.service = service;
         this.categoriaDespesaService = categoriaDespesaService;
         this.formaPagamentoService = formaPagamentoService;
+        linhasSelecionadas = new HashSet<>();
+        movimentosSnapShot = new ArrayList<>();
         
 
     }
@@ -90,16 +103,26 @@ public class DespesaViewController implements AppViewController<DespesaView> {
     @Override
     public void limparCampos() {
         view.getTextFields().stream().forEach(f -> f.setText(""));
-        view.getSpinnerQuantidadeParcelas().getModel().setValue(0);
+        view.getSpinnerQuantidadeParcelas().getModel().setValue(1);
         limparTabela();
     }
 
-    public Despesa build() {
-        DespesaAvulsa despesa = new DespesaAvulsa();
-
-        return null;
+    /** 
+     * Constroi o objeto que será salvo no banco de dados.
+     */
+    public Despesa getInstance(String type) {
+       
+        if(type.equals(DESPESA_AVULA)){
+            return new DespesaAvulsa();
+        
+        }else
+            return new DespesaRecorrente();
+       
     }
 
+    /**
+     * Atualiza os combobox da view com os valores vindos do banco de dados.
+     */
     public void atualizarCampos() {
         addValoresComboCategoria();
         addValoresComboFormaPagamento();
@@ -107,26 +130,35 @@ public class DespesaViewController implements AppViewController<DespesaView> {
 
     }
 
+    /**
+     * Adiciona as categorias na combobox de categorias da view.
+     */
     private void addValoresComboCategoria() {
 
         this.categoriaDespesaService.getTodos().stream().forEach(c -> {
-            System.out.println(c.getName());
             view.getComboCategoria().addItem(c.getName());
         });
 
         view.getComboCategoria().setSelectedIndex(-1);
     }
+    
 
+    /**
+     * Adiciona as formas de pagamento nas combox da view.
+     */
     private void addValoresComboFormaPagamento() {
 
         this.formaPagamentoService.getTodos().stream().forEach(f -> {
-            System.out.println(f.getName());
             view.getComboFormaPagamento().addItem(f.getName());
+            view.getComboFormaPagamentoTabela().addItem(f.getName());
         });
 
         view.getComboFormaPagamento().setSelectedIndex(-1);
     }
 
+    /**
+     * Adiciona os valores das formas de parcelamneto na combo da view.
+     */
     private void addValoresComboParcelamento() {
             
         Arrays.asList(Periodo.values()).stream().forEach(p -> 
@@ -134,6 +166,11 @@ public class DespesaViewController implements AppViewController<DespesaView> {
         view.getComboParcelamento().setSelectedIndex(-1);
     }
 
+    
+    /**
+     * Ativa ou desativa os componentes da view.
+     * @param con boolean 'true' para ativar 'false' para desativar
+     */
     public void enableDisableComponents(boolean con) {
 
         view.listaDeComponentes().stream().forEach(c -> {
@@ -144,33 +181,50 @@ public class DespesaViewController implements AppViewController<DespesaView> {
 
     }
 
+    /**
+     * Cria os movimentos/parcelas de acordo com os valores adicionados nos campos
+     * referentes as parcelas na view.
+     */
     public void criarMovimentos() {
         GeradorMovimentos gm = new GeradorMovimentos();
         
-        List<MovimentoPagamento> movimentos = gm.gerarMovimentos(
+        movimentosSnapShot = gm.gerarMovimentos(
                 (String) view.getComboParcelamento().getSelectedItem(),
                 (int)view.getSpinnerQuantidadeParcelas().getValue(),
                 view.getFieldVencimentoParcela().getText(),
                 view.getFieldValorTotal().getText(),
-                new FormaPagamento());
+                formaPagamentoService.getByForma(String.valueOf(view.getComboFormaPagamento().getModel().getSelectedItem())));
         
         
-        atualizarTabela(movimentos);
+        atualizarTabela(movimentosSnapShot);
     }
     
+    
+    /**
+     * Limpa todos os valores que estão dentro da tabela.
+     */
     public void limparTabela(){
         DefaultTableModel model = (DefaultTableModel) view.getTableParcelas().getModel();
         ControllerHelper.limparTabela(model);
     
     }
 
+    
+    /** 
+     * Atualiza os valores da tabela a partir de uma lista de movimentos/parcelas
+     * que forem passadas.
+     * @param movimentos
+     * Lista de movimentos que estão salvo ou não no banco de dados
+     * que irão preencher a tabela.
+     */
     public void atualizarTabela(List<MovimentoPagamento> movimentos) {
         
         DefaultTableModel model = (DefaultTableModel) view.getTableParcelas().getModel();
         ControllerHelper.limparTabela(model);
         
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        NumberFormat formatoMoeda = NumberFormat.getCurrencyInstance();
+        Locale localBrasil = new Locale("pt", "BR");
+        NumberFormat formatoMoeda = NumberFormat.getCurrencyInstance(localBrasil);
         
         movimentos.stream().forEach(m -> {
             model.addRow(new Object[]{m.getId(),
@@ -181,5 +235,17 @@ public class DespesaViewController implements AppViewController<DespesaView> {
                 m.getFormaPagamento().getName()});
         });
     }
+    
+    /**
+     * Adiciona as linhas que foram selecionadas na tabela e que podem ter
+     * sofrido alguma alteração nos seus valores, para que esses novos valores
+     * alterados possam ser atualizados na lista salva em memoria.
+     * @param indexLinha 
+     */
+    public void adicionarLinhaAlterada(int indexLinha){
+        linhasSelecionadas.add(indexLinha);
+    }
 
+    
+      
 }
