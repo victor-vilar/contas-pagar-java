a8d02024c358f55f9cee575ed83fe4ff49c2c2cf
Victor Vilar <victor.svilar@gmail.com>
2025-04-01 18:43:53 -0300

    feat:Criada classe ConversorMoeda para transormar os valores em texto em bigDecimal e vice versa.

---
+++ src/main/java/br/com/thveiculos/erp/services/despesas/implementation/ConversorMoeda.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.thveiculos.erp.services.despesas.implementation;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+/**
+ *
+ * @author victor
+ */
+public abstract class ConversorMoeda {
+
+    private static NumberFormat formater = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+
+    /**
+     * Converte o texto que vai estar no formato 'R$1.000,00' para um bigDecimal.
+     * @param moeda Representação da moeda em string;
+     * @return BigDecimal do valor passado
+     * @throws ParseException 
+     */
+    public static BigDecimal paraBigDecimal(String moeda) throws ParseException {
+
+           moeda = moeda.replaceAll("[^0-9,]", "");
+           moeda = moeda.replace(",",".");
+           return new BigDecimal(moeda);
+    }
+
+    
+    /**
+     * Converte um bigDecimal para uma String no formato 'R$1.000,00'.
+     * @param moeda
+     * @return
+     * @throws ParseException 
+     */
+    public static String paraString(BigDecimal moeda) throws ParseException {
+        String texto = formater.format(moeda);
+        return texto.replace("\\s+","");
+    }
+
+}
