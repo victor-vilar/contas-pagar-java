4ce239f542e071eba7aefdb1ce3a0d4b5afe9e51
Victor Vilar <victor.svilar@gmail.com>
2025-04-07 17:28:45 -0300

    feat:Adicionando campo de formaPagamentoPadrao na classe DespesaRecorrente, para que esse valor possa ser passado para os movimentos.

--- src/main/java/br/com/thveiculos/erp/entities/despesas/DespesaRecorrente.java
+++ src/main/java/br/com/thveiculos/erp/entities/despesas/DespesaRecorrente.java
@@ -6,57 +6,68 @@ import java.time.LocalDate;
 import br.com.thveiculos.erp.enums.despesas.Periodo;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="despesas_recorrentes")
+@Table(name = "despesas_recorrentes")
 public class DespesaRecorrente extends DespesaAbstrata implements Serializable {
 
+    @Column(nullable = false)
+    private Periodo periocidade;
+    @Column(nullable = false)
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+    @Column(nullable = false)
+    private Integer dataPagamento;
 
-	@Column(nullable=false)
-	private Periodo periocidade;
-	@Column(nullable=false)
-	private LocalDate dataInicio;
-	private LocalDate dataFim;
-        @Column(nullable=false)
-	private Integer dataPagamento;
-	
-	
-	public DespesaRecorrente() {
-		super();
-	}
-	
+    @ManyToOne
+    @JoinColumn(name = "forma_pagamento_pradrão")
+    private FormaPagamento formaPagamentoPadrao;
 
-	public Periodo getPeriocidade() {
-		return periocidade;
-	}
-	public void setPeriocidade(Periodo periocidade) {
-		this.periocidade = periocidade;
-	}
-	public LocalDate getDataInicio() {
-		return dataInicio;
-	}
-	public void setDataInicio(LocalDate dataInicio) {
-		this.dataInicio = dataInicio;
-	}
-	public LocalDate getDataFim() {
-		return dataFim;
-	}
-	public void setDataFim(LocalDate dataFim) {
-		this.dataFim = dataFim;
-	}
-	public Integer getDataPagamento() {
-		return dataPagamento;
-	}
-	public void setDataPagamento(Integer dataPagamento) {
-		this.dataPagamento = dataPagamento;
-	}
-	
+    public DespesaRecorrente() {
+        super();
+    }
 
-	
+    public Periodo getPeriocidade() {
+        return periocidade;
+    }
 
+    public void setPeriocidade(Periodo periocidade) {
+        this.periocidade = periocidade;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public Integer getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(Integer dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    public FormaPagamento getFormaPagamentoPadrao() {
+        return formaPagamentoPadrao;
+    }
+
+    public void setFormaPagamentoPadrao(FormaPagamento formaPagamentoPadrao) {
+        this.formaPagamentoPadrao = formaPagamentoPadrao;
+    }
 
 }
