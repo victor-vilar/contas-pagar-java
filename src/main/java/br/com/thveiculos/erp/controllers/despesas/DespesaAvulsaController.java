9ef5f5da31a8529169bffc63d8acfe419dcf73b3
Victor Vilar <victor.svilar@gmail.com>
2025-04-08 13:57:53 -0300

    feat:Alterada classe DespesaAvulsaController que agora extends de DespesaViewAbstractController e implementa trechos de código para o seu tipo de view.

--- src/main/java/br/com/thveiculos/erp/controllers/despesas/DespesaAvulsaController.java
+++ src/main/java/br/com/thveiculos/erp/controllers/despesas/DespesaAvulsaController.java
@@ -5,10 +5,13 @@
 package br.com.thveiculos.erp.controllers.despesas;
 
 import br.com.thveiculos.erp.controllers.AppViewController;
+import br.com.thveiculos.erp.entities.despesas.DespesaAvulsa;
+import br.com.thveiculos.erp.entities.despesas.NotaFiscal;
 import br.com.thveiculos.erp.services.despesas.interfaces.CategoriaDespesaService;
 import br.com.thveiculos.erp.services.despesas.interfaces.DespesaService;
 import br.com.thveiculos.erp.services.despesas.interfaces.FormaPagamentoService;
-import br.com.thveiculos.erp.views.despesas.DespesaAvulsaView;
+import br.com.thveiculos.erp.util.ConversorData;
+import br.com.thveiculos.erp.views.despesas.DespesaAvulsaViewImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
@@ -19,11 +22,80 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 @Lazy
-public class DespesaAvulsaController extends DespesaViewController implements AppViewController<DespesaAvulsaView> {
+public class DespesaAvulsaController extends DespesaViewAbstractController<DespesaAvulsaViewImpl> implements AppViewController<DespesaAvulsaViewImpl> {
+    
+    
     
     @Autowired
     public DespesaAvulsaController(DespesaService service, CategoriaDespesaService categoriaDespesaService, FormaPagamentoService formaPagamentoService) {
         super(service, categoriaDespesaService, formaPagamentoService);
     }
     
+    @Override
+    public void setView(DespesaAvulsaViewImpl view) {
+        this.view = view;
+    }
+
+    @Override
+    public void salvar() {
+        DespesaAvulsa despesa = new DespesaAvulsa();
+
+        String numeroNota = view.getFieldNota().getText();
+        String dataNota = view.getFieldNotaEmissao().getText();
+        String id = view.getFieldId().getText();
+
+        if (!id.equals("")) {
+            despesa.setId(Long.valueOf(id));
+        }
+
+        if (!numeroNota.equals("") && !dataNota.equals("")) {
+            despesa.setNotaFiscal(buildNota(numeroNota, dataNota));
+        }
+
+        String nome = view.getFieldDescricao().getText();
+        String descricao = view.getAreaDescricao().getText();
+        String categoria = (String) view.getComboCategoria().getSelectedItem();
+
+        despesa.setNomeFornecedor(nome);
+        despesa.setDescricao(descricao);
+        despesa.setCategoria(this.categoriaDespesaService.getByCategoria(categoria));
+        despesa.setParcelas(this.movimentos);
+
+        this.service.save(despesa);
+        limparCampos();
+
+    }
+
+    private NotaFiscal buildNota(String numero, String data) {
+
+        NotaFiscal nota = new NotaFiscal();
+        nota.setDataEmissao(ConversorData.paraData(data));
+        nota.setNumero(numero);
+        return nota;
+    }
+    
+    public void gerarParcelas() {
+        criarMovimentos();
+        preencherTabela(movimentos);
+        limparCamposParcelamento();
+    }
+
+    private void criarMovimentos() {
+
+        movimentos = service.gerarMovimentos(
+                (String) view.getComboParcelamento().getSelectedItem(),
+                (int) view.getSpinnerQuantidadeParcelas().getValue(),
+                view.getFieldVencimento().getText(),
+                view.getFieldValor().getText(),
+                formaPagamentoService.getByForma((String) view.getComboFormaPagamento().getSelectedItem()));
+
+    }
+    
+    @Override
+    protected void limparCamposParcelamento() {
+        super.limparCamposParcelamento();
+        view.getSpinnerQuantidadeParcelas().getModel().setValue(1);
+        view.getFieldVencimento().setText("");
+
+    }
 }
