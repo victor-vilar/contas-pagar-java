37891153fe60334180c2480fe99a6cc28dc89a3d
Victor Vilar <victor.svilar@gmail.com>
2025-04-09 19:22:36 -0300

    feat/style:Alterado layout do formulário e adicinado botões para outros formulários.

--- src/main/java/br/com/thveiculos/erp/views/util/MainView.java
+++ src/main/java/br/com/thveiculos/erp/views/util/MainView.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Component;
 
 import br.com.thveiculos.erp.views.despesas.CategoriaDespesaView;
 import br.com.thveiculos.erp.views.despesas.DespesaAvulsaViewImpl;
+import br.com.thveiculos.erp.views.despesas.DespesaRecorrenteViewImpl;
+import br.com.thveiculos.erp.views.despesas.FormaPagamentoView;
 import br.com.thveiculos.erp.views.despesas.MovimentoPagamentoView;
 
 @Component
@@ -25,6 +27,8 @@ public class MainView extends JFrame {
 	private JButton btnFormaPagamento;
 	private JButton btnCategoriaDespesa;
         private JButton btnMovimentoPagamento;
+        private JButton btnDespesaAvulsa;
+        private JButton btnDespesaRecorrente;
 
 	@Autowired
 	public void MainView(ApplicationContext context) {
@@ -36,47 +40,58 @@ public class MainView extends JFrame {
 	 * Create the frame.
 	 */
 	public void setUp() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 476, 345);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setBounds(100, 100, 476, 345);
+            contentPane = new JPanel();
+            contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+            setContentPane(contentPane);
 
-		
-		btnFormaPagamento = new JButton("DespesaView");
-		btnFormaPagamento.setSize(177, 50);
-		btnFormaPagamento.setLocation(10, 10);
-		btnFormaPagamento.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				var view = context.getBean(DespesaAvulsaViewImpl.class);
-				view.setVisible(true);
-			}
-		});
-		
-		btnCategoriaDespesa = new JButton("Categorias Despesa");
-		btnCategoriaDespesa.setSize(151, 50);
-		btnCategoriaDespesa.setLocation(199, 10);
-		btnCategoriaDespesa.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				var view = context.getBean(CategoriaDespesaView.class);
-				view.setVisible(true);
-			}
-		});
-                
-                btnMovimentoPagamento = new JButton("MovimentoPagamento");
-                btnMovimentoPagamento.setSize(151, 50);
-                btnCategoriaDespesa.setLocation(199, 80);
-                btnMovimentoPagamento.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				var view = context.getBean(MovimentoPagamentoView.class);
-				view.setVisible(true);
-			}
-		});
+            btnFormaPagamento = new JButton("Forma Pagamento");
+            btnFormaPagamento.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent arg0) {
+                    var view = context.getBean(FormaPagamentoView.class);
+                    view.setVisible(true);
+                }
+            });
+
+            btnCategoriaDespesa = new JButton("Categorias Despesa");
+            btnCategoriaDespesa.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent arg0) {
+                    var view = context.getBean(CategoriaDespesaView.class);
+                    view.setVisible(true);
+                }
+            });
+
+            btnMovimentoPagamento = new JButton("MovimentoPagamento");
+            btnMovimentoPagamento.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent arg0) {
+                    var view = context.getBean(MovimentoPagamentoView.class);
+                    view.setVisible(true);
+                }
+            });
+ 
+            btnDespesaAvulsa = new JButton("Despesa Avulsa");
+            btnDespesaAvulsa.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent arg0) {
+                    var view = context.getBean(DespesaAvulsaViewImpl.class);
+                    view.setVisible(true);
+                }
+            });
+            
+            btnDespesaRecorrente = new JButton("Despesa Recorrente");
+            btnDespesaRecorrente.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent arg0) {
+                    var view = context.getBean(DespesaRecorrenteViewImpl.class);
+                    view.setVisible(true);
+                }
+            });
 		
 		contentPane.add(btnFormaPagamento);
 		contentPane.add(btnCategoriaDespesa);
+                contentPane.add(btnMovimentoPagamento);
+                contentPane.add(btnDespesaAvulsa);
+                contentPane.add(btnDespesaRecorrente);
                 
 		
 		
