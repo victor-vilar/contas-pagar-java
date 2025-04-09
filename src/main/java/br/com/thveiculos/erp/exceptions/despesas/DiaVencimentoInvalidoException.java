0cd3e188cbaa5ae30dd9350b305083c6011c8946
Victor Vilar <victor.svilar@gmail.com>
2025-04-09 19:16:40 -0300

    feat:Criada as classes DiaVencimentoInvalidoException e MesVencimentoInvalidoException.

---
+++ src/main/java/br/com/thveiculos/erp/exceptions/despesas/DiaVencimentoInvalidoException.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.thveiculos.erp.exceptions.despesas;
+
+/**
+ *
+ * @author victor
+ */
+public class DiaVencimentoInvalidoException extends RuntimeException {    
+    
+    public DiaVencimentoInvalidoException(String msg) {
+        super(msg);
+    }
+    
+    
+    
+}
