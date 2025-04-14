a543a8df7631b4541fed348fd6c89abde2bc7033
Victor Vilar <victor.svilar@gmail.com>
2025-04-14 15:48:13 -0300

    feat:Criado metodo na interface MovimentoPagamentoRepository para buscar Movimentos que estão e não estão pagos.

--- src/main/java/br/com/thveiculos/erp/repositories/despesas/MovimentoPagamentoRepository.java
+++ src/main/java/br/com/thveiculos/erp/repositories/despesas/MovimentoPagamentoRepository.java
@@ -3,7 +3,10 @@ package br.com.thveiculos.erp.repositories.despesas;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.thveiculos.erp.entities.despesas.MovimentoPagamento;
+import java.util.List;
 
 public interface MovimentoPagamentoRepository extends JpaRepository<MovimentoPagamento, Long> {
 
+    List<MovimentoPagamento> findByDataPagamentoIsNull();
+    List<MovimentoPagamento> findByDataPagamentoIsNotNull();
 }
