2c32a4f758ac0ea4a9d73813db58879e7f94c44a
Victor Vilar <victor.svilar@gmail.com>
2025-04-14 15:15:07 -0300

    fix:Adicionado método getMovimentosDeletados() no MovimentoPagamentoService.

--- src/main/java/br/com/thveiculos/erp/services/despesas/interfaces/MovimentoPagamentoService.java
+++ src/main/java/br/com/thveiculos/erp/services/despesas/interfaces/MovimentoPagamentoService.java
@@ -6,7 +6,8 @@ import java.util.List;
 import javax.swing.table.DefaultTableModel;
 
 public interface MovimentoPagamentoService extends AppService<MovimentoPagamento>{
-       
+    
+    public List<MovimentoPagamento> getMovimentosDeletados();
     public List<MovimentoPagamento> gerarMovimentos(String parcelamento,int qtdParcelas, String dataInicial, String valor, FormaPagamento formaPagamento);
     public void atualizarMovimentos(List<MovimentoPagamento> movimentos, int linha, DefaultTableModel model);
     public void deletarMovimentos(List<MovimentoPagamento> movimentos, int[] linhas);
