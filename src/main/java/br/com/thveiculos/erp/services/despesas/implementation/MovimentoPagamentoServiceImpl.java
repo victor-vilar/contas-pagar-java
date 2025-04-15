13ee9ba164765c1d6bc175a8499f73615f9a3445
Victor Vilar <victor.svilar@gmail.com>
2025-04-15 13:42:18 -0300

    Implementado método de getById em MovimentoPagamentoServiceImpl.

--- src/main/java/br/com/thveiculos/erp/services/despesas/implementation/MovimentoPagamentoServiceImpl.java
+++ src/main/java/br/com/thveiculos/erp/services/despesas/implementation/MovimentoPagamentoServiceImpl.java
@@ -160,7 +160,7 @@ public class MovimentoPagamentoServiceImpl implements MovimentoPagamentoService 
 
     @Override
     public MovimentoPagamento getById(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Não encontrado"));
     }
 
     @Override
