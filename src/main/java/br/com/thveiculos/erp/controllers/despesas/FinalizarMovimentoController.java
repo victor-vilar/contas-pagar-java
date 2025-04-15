9418eb767cb9958a77e3dc8d61cc242effafb16e
Victor Vilar <victor.svilar@gmail.com>
2025-04-15 12:31:58 -0300

    feat:Criado classe FinlizarMovimentoController.

---
+++ src/main/java/br/com/thveiculos/erp/controllers/despesas/FinalizarMovimentoController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.thveiculos.erp.controllers.despesas;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author victor
+ */
+@Controller
+@Lazy
+public class FinalizarMovimentoController {
+    
+}
