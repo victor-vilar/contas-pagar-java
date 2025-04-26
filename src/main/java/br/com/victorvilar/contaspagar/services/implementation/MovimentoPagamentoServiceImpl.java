51cb237740d65eebc784d18e4eb7ffc0762db910
Victor Vilar <victor.svilar@gmail.com>
2025-04-26 11:30:50 -0300

    fix:removido metodo de MovimentoPagamentoServiceImpl.

--- src/main/java/br/com/victorvilar/contaspagar/services/implementation/MovimentoPagamentoServiceImpl.java
+++ src/main/java/br/com/victorvilar/contaspagar/services/implementation/MovimentoPagamentoServiceImpl.java
@@ -31,148 +31,14 @@ import org.springframework.stereotype.Service;
 public class MovimentoPagamentoServiceImpl implements MovimentoPagamentoService {
 
     private final MovimentoPagamentoRepository repository;
-    private final FormaPagamentoService formaPagamentoService;
     private List<MovimentoPagamento> movimentosDeletados = new ArrayList<>();
     private List<MovimentoPagamento> movimentosAtualizados = new ArrayList<>();
-    
+
+
 
     @Autowired
-    public MovimentoPagamentoServiceImpl(MovimentoPagamentoRepository repository, FormaPagamentoService formaPagamentoService){
+    public MovimentoPagamentoServiceImpl(MovimentoPagamentoRepository repository){
         this.repository = repository;
-        this.formaPagamentoService = formaPagamentoService;
-    }
-    
-    public void clearList(){
-        movimentosDeletados.clear();
-        movimentosAtualizados.clear();
-    }
-    
-    public List<MovimentoPagamento> getMovimentosDeletados() {
-        return movimentosDeletados;
-    }
-    
-    public List<MovimentoPagamento> getMovimentosAtualizados(){
-        return movimentosAtualizados;
-    }
-
-    public List<MovimentoPagamento> gerarMovimentos(String parcelamento, int qtdParcelas, String dataInicial, String valor, FormaPagamento formaPagamento)
-            throws QuantidadeDeParcelasException, DateTimeParseException {
-
-        if (qtdParcelas <= 0) {
-            throw new QuantidadeDeParcelasException();
-        }
-
-        LocalDate data = ConversorData.paraData(dataInicial);
-        List<MovimentoPagamento> movimentos = new ArrayList<>();
-
-        //Se a data passada for dia 31, ira configurar para o dia 30
-        if (data.getDayOfMonth() == 31) {
-            data = data.minusDays(1);
-        }
-
-        for (int i = 0; i < qtdParcelas; i++) {
-
-            MovimentoPagamento mp = new MovimentoPagamento();
-            mp.setValorPagamento(ConversorMoeda.paraBigDecimal(valor));
-            mp.setFormaPagamento(formaPagamento);
-
-            if (i != 0) {
-                data = dataProximaParcela(parcelamento, data);
-            }
-
-            mp.setDataVencimento(data);
-            movimentos.add(mp);
-        }
-        adicionarOuAtualizarReferenteParcela(movimentos);
-        return movimentos;
-    }
-
-    /**
-     * Adiciona no numero das parcelas ou atualiza caso já existam.
-     * @param movimentos
-     */
-    public void adicionarOuAtualizarReferenteParcela(List<MovimentoPagamento> movimentos){
-
-        int quantidade = movimentos.size();
-
-        for(int i = 0; i < quantidade ; i++){
-
-            if(quantidade == 1){
-                movimentos.get(0).setReferenteParcela("UNICA");
-                break;
-            }
-
-            movimentos.get(i).setReferenteParcela(i+1 +"/" + quantidade);
-
-        }
-    }
-
-    private LocalDate dataProximaParcela(String parcelamento, LocalDate dataAtual) {
-
-        LocalDate novaData;
-
-        switch (parcelamento) {
-            case "ANUAL":
-                novaData = dataAtual.plusYears(1);
-                break;
-            case "MENSAL":
-                novaData = dataAtual.plusMonths(1);
-                break;
-            case "QUINZENAL":
-                novaData = dataAtual.plusDays(15);
-                break;
-            case "SEMANAL":
-                novaData = dataAtual.plusWeeks(1);
-                break;
-            default:
-                novaData = dataAtual;
-                break;
-        }
-
-        return novaData;
-    }
-
-    public void atualizarMovimentosTabela(List<MovimentoPagamento> movimentos, int linha, DefaultTableModel model) {
-
-        //Busca o movimento na lista. O movimento se encontra na mesma posição da linha
-        MovimentoPagamento mp = movimentos.get(linha);
-        
-        //Atualiza a data de vencimento 
-        mp.setDataVencimento(ConversorData.paraData(String.valueOf(model.getValueAt(linha, 2))));
-
-        //Atualiza o valor de pagamento            
-        mp.setValorPagamento(ConversorMoeda.paraBigDecimal(String.valueOf(model.getValueAt(linha, 3))));
-
-        //Atualiza da data de pagmento
-        mp.setDataPagamento(ConversorData.paraData(String.valueOf(model.getValueAt(linha, 4))));
-        
-        //Atualiza a forma de pagamento
-        mp.setFormaPagamento(formaPagamentoService.getByForma(String.valueOf(model.getValueAt(linha,5))));
-        
-        //Atualiza observaçao
-        mp.setObservacao((String)model.getValueAt(linha, 6));
-        
-        movimentosAtualizados.add(mp);
-
-    }
-
-    /**
-     * Deleta os movimentos da lista, de acordo com as linhas deletadas da
-     * tabela. Atualiza a propriedade referenteParcela do movimento .
-     */
-    public void deletarMovimentosTabela(List<MovimentoPagamento> movimentos, int[] linhas) {
-
-        //deleta os movimentos da lista que estão nas mesma posiçaõ das linhas
-        //adiciona movimentos deletados na lista para que possam ser excluidos
-        //caso já tenham sido salvos no banco.
-        //O código começa a eliminar a partir do fim da fila, pois os elementos
-        //mudam de posição quando são removidos
-        for (int i = linhas.length - 1; i >= 0; i--) {
-            movimentosDeletados.add(movimentos.remove(linhas[i]));
-        }
-
-        adicionarOuAtualizarReferenteParcela(movimentos);
-
     }
 
     @Override
@@ -227,17 +93,11 @@ public class MovimentoPagamentoServiceImpl implements MovimentoPagamentoService 
      */
     @Transactional
     public void update(){
-        
-        if(!movimentosDeletados.isEmpty()){
-           deleteAll(movimentosDeletados);
-        }
-        
+
         if(!movimentosAtualizados.isEmpty()){
            update(movimentosAtualizados); 
         }
 
-       clearList();
-        
     }
 
     @Override
