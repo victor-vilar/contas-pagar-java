94474bf182bc26ee540b505f55a055601fbf7e8b
Victor Vilar <victor.svilar@gmail.com>
2025-04-28 14:56:13 -0300

    feat:Criado query para poder buscar as DespesasAbstratas com as suas parcelas.

--- src/main/java/br/com/victorvilar/contaspagar/repositories/DespesaRepository.java
+++ src/main/java/br/com/victorvilar/contaspagar/repositories/DespesaRepository.java
@@ -3,7 +3,11 @@ package br.com.victorvilar.contaspagar.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.victorvilar.contaspagar.entities.DespesaAbstrata;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DespesaRepository extends JpaRepository<DespesaAbstrata, Long> {
 
+    @Query("SELECT d FROM DespesaAbstrata d LEFT JOIN FETCH d.movimentos WHERE d.id = :id")
+    DespesaAbstrata findByIdWithMovimentos(@Param("id")Long id);
 }
