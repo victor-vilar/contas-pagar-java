431e39a15c2c9fd80cf9fe9cf41eb79034f4bf31
Victor Vilar <victor.svilar@gmail.com>
2025-04-28 14:58:25 -0300

    fix:Removida anotações @Transactional de appService, pois elas so funcionam em beans.

--- src/main/java/br/com/victorvilar/contaspagar/services/interfaces/AppService.java
+++ src/main/java/br/com/victorvilar/contaspagar/services/interfaces/AppService.java
@@ -8,15 +8,10 @@ public interface AppService<T> {
 	
 	public List<T> getTodos();
 	public T getById(Long id);
-        @Transactional
 	public T save(T obj);
-	@Transactional
-        public List<T> saveAll(List<T> objs);
-	@Transactional
-        public T update(T obj);
-	@Transactional
-        public void deleteById(Long id);
-	@Transactional
-        public void deleteAll(List<T> objs);
+	public List<T> saveAll(List<T> objs);
+	public T update(T obj);
+	public void deleteById(Long id);
+	public void deleteAll(List<T> objs);
 	
 }
