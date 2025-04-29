4bc613174123d30beb3dd3b6e9f71cde99365e73
Victor Vilar <victor.svilar@gmail.com>
2025-04-29 16:06:55 -0300

    feat:Criada classe para agrupar as mensagens constantes da aplicação.

---
+++ src/main/java/br/com/victorvilar/contaspagar/util/AppMensagens.java
@@ -0,0 +1,23 @@
+package br.com.victorvilar.contaspagar.util;
+
+/**
+ *
+ */
+public abstract class AppMensagens {
+
+    public static final String HEADER_SUCESSO="Sucesso";
+    public static final String HEADER_ERRO="Erro";
+    public static final String HEADER_ATENCAO="Atenção";
+    
+    
+    public static final String SUCESSO = "Operação realiazada com sucesso !";
+    
+    public static final String DATA_INCORRETA = "A data informada não está correta !";
+    public static final String VALOR_INCORRETO = "O valor informado não é um número válido !"; 
+    public static final String ERRO_INESPERADO = "Ocorreu um erro inesperado !";
+    
+    public static final String REMOVER_ITENS ="Deseja remover os itens selecionados ?";
+    
+    
+
+}
