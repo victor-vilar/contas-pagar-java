593375eb48c4c69cf540d03552595e2d1acfcf98
Victor Vilar <victor.svilar@gmail.com>
2025-04-30 14:31:23 -0300

    doc:Alterado nome de algumas propriedades para ficar mais facil identifica-las.

--- src/main/java/br/com/victorvilar/contaspagar/util/AppMensagens.java
+++ src/main/java/br/com/victorvilar/contaspagar/util/AppMensagens.java
@@ -5,18 +5,19 @@ package br.com.victorvilar.contaspagar.util;
  */
 public abstract class AppMensagens {
 
+    //HEADERS
     public static final String HEADER_SUCESSO="Sucesso";
     public static final String HEADER_ERRO="Erro";
     public static final String HEADER_ATENCAO="Atenção";
-    
-    
-    public static final String SUCESSO = "Operação realiazada com sucesso !";
-    
-    public static final String DATA_INCORRETA = "A data informada não está correta !";
-    public static final String VALOR_INCORRETO = "O valor informado não é um número válido !"; 
-    public static final String ERRO_INESPERADO = "Ocorreu um erro inesperado !";
-    
+
+    //ERROS
+    public static final String INFO_DATA_INCORRETA = "A data informada não está correta !";
+    public static final String INFO_VALOR_INCORRETO = "O valor informado não é um número válido !";
+    public static final String INFO_ERRO_INESPERADO = "Ocorreu um erro inesperado !";
+
+    public static final String INFO_SUCESSO = "Operação realizada com sucesso !";
     public static final String REMOVER_ITENS ="Deseja remover os itens selecionados ?";
+
     
     
 
