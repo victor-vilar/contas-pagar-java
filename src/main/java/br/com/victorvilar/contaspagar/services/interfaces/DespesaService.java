6dff00549c9863f2c97c31ff4955724800ef4ed1
Victor Vilar <victor.svilar@gmail.com>
2025-05-01 12:51:40 -0300

    feat:Adicionado metodo para buscar as despesas com os movimentos pelo id da despeas.

--- src/main/java/br/com/victorvilar/contaspagar/services/interfaces/DespesaService.java
+++ src/main/java/br/com/victorvilar/contaspagar/services/interfaces/DespesaService.java
@@ -7,5 +7,7 @@ import java.util.List;
 import javax.swing.table.DefaultTableModel;
 
 public interface DespesaService extends AppService<DespesaAbstrata>{
+    
+    public DespesaAbstrata findByIdWithMovimentos(Long id);
 
 }
