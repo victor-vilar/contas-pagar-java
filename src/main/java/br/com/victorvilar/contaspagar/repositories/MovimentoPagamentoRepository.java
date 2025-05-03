bc1c3b459040aa31a4ae98b9697302a3a9b485ce
Victor Vilar <victor.svilar@gmail.com>
2025-05-03 13:00:40 -0300

    feat:Criado metodos para buscar despesas pagas e não pagas por periodo e pela descricao da despesa.

--- src/main/java/br/com/victorvilar/contaspagar/repositories/MovimentoPagamentoRepository.java
+++ src/main/java/br/com/victorvilar/contaspagar/repositories/MovimentoPagamentoRepository.java
@@ -3,7 +3,9 @@ package br.com.victorvilar.contaspagar.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.victorvilar.contaspagar.entities.MovimentoPagamento;
+import java.time.LocalDate;
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MovimentoPagamentoRepository extends JpaRepository<MovimentoPagamento, Long> {
 
@@ -11,4 +13,10 @@ public interface MovimentoPagamentoRepository extends JpaRepository<MovimentoPag
     List<MovimentoPagamento> findByDataPagamentoIsNotNull();
 
     public List<MovimentoPagamento> getAllByDespesaId(Long id);
+    
+    @Query("SELECT m FROM MovimentoPagamento m WHERE LOWER(m.despesa.nomeFornecedor) LIKE LOWER(CONCAT('%', :despesaName, '%')) AND dataVencimento >= :dataInicio AND dataVencimento <= :dataFim AND dataPagamento IS NULL")
+    List<MovimentoPagamento> getBetweenDatesAndDespesaNameNaoPago(LocalDate dataInicio, LocalDate dataFim, String despesaName);
+    
+    @Query("SELECT m FROM MovimentoPagamento m WHERE LOWER(m.despesa.nomeFornecedor) LIKE LOWER(CONCAT('%', :despesaName, '%')) AND dataVencimento >= :dataInicio AND dataVencimento <= :dataFim AND dataPagamento IS NOT NULL")
+    List<MovimentoPagamento> getBetweenDatesAndDespesaNamePago(LocalDate dataInicio, LocalDate dataFim, String despesaName);
 }
