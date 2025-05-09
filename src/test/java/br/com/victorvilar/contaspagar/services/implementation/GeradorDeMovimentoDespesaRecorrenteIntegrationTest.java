6f829e6f9f620986f0fb897a975daed0dd38bab4
Victor Vilar <victor.svilar@gmail.com>
2025-05-09 12:59:57 -0300

    test:Criado mais testes.

--- src/test/java/br/com/victorvilar/contaspagar/services/implementation/GeradorDeMovimentoDespesaRecorrenteIntegrationTest.java
+++ src/test/java/br/com/victorvilar/contaspagar/services/implementation/GeradorDeMovimentoDespesaRecorrenteIntegrationTest.java
@@ -1,8 +1,6 @@
 package br.com.victorvilar.contaspagar.services.implementation;
 
-import br.com.victorvilar.contaspagar.repositories.DespesaRepository;
 import br.com.victorvilar.contaspagar.repositories.MovimentoPagamentoRepository;
-import net.bytebuddy.utility.dispatcher.JavaDispatcher;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,33 +8,38 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
+//Spring boot test falo para iniciar o contexto do spring no teste
 @SpringBootTest
 @Testcontainers
 @ActiveProfiles("test")
-public class LancamentoMovimentoDespesaRecorrenteIntegrationTest {
+public class GeradorDeMovimentoDespesaRecorrenteIntegrationTest {
 
     @Autowired
     private GeradorDeMovimentoDespesaRecorrente gerador;
 
+    @Autowired
+    private MovimentoPagamentoRepository repository;
+
     @Container
-    PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("pgvector/pgvector:pg16");
-            
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("pgvector/pgvector:pg16")
+            .withDatabaseName("teste")
+            .withUsername("teste")
+            .withPassword("teste");
 
-    @BeforeEach
-    public void setUp(){
-        postgres.start();
+
+    //passa configurações dinamicas para o spring ao invez dele puxar as configurações do application.properties
+    @DynamicPropertySource
+    public static void overrideProps(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url",postgres::getJdbcUrl);
+        registry.add("spring.datasource.useraname",postgres::getUsername);
+        registry.add("spring.datasource.password",postgres::getPassword);
     }
 
-    @AfterEach
-    public void endUp(){
-        postgres.stop();
-    }
+
 
 
 
