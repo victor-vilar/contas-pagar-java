7f10b5198525cfa0cc9f944ebb1fd64c92933769
Victor Vilar <victor.svilar@gmail.com>
2025-05-14 18:37:56 -0300

    feat:Adicionanda mais uma mensagem na classe AppMensagens.

--- src/main/java/br/com/victorvilar/contaspagar/util/AppMensagens.java
+++ src/main/java/br/com/victorvilar/contaspagar/util/AppMensagens.java
@@ -15,6 +15,7 @@ public abstract class AppMensagens {
     public static final String INFO_VALOR_INCORRETO = "O valor informado não é um número válido !";
     public static final String INFO_ERRO_INESPERADO = "Ocorreu um erro inesperado !";
     public static final String INFO_ERRO_ENTIDADE_JA_EXISTE = "Não foi possível salvar pois já existe um item com o mesmo nome !";
+    public static final String INFO_PREENCHER_TODOS_CAMPOS = "Todos os campos com (*) devem ser preenchidos !";
 
     public static final String INFO_SUCESSO = "Operação realizada com sucesso !";
     public static final String REMOVER_ITENS ="Deseja remover os itens selecionados ?";
