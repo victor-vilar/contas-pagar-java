dd39b629354df0b432968846beb2a6aa4382ed7b
Victor Vilar <victor.svilar@gmail.com>
2025-06-02 17:08:49 -0300

    feat:Criado exception MovimentosPeriodoVazio para lançar quando um perido pesquisado não tiver movimentos.

---
+++ src/main/java/br/com/victorvilar/contaspagar/exceptions/MovimentosPeriodoVazio.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.victorvilar.contaspagar.exceptions;
+
+/**
+ *
+ * @author victor
+ */
+public class MovimentosPeriodoVazio extends RuntimeException {
+    
+    
+    public MovimentosPeriodoVazio(String msg){
+        super(msg);
+    }
+    
+}
