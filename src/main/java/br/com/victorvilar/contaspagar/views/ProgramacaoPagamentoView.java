0baccf5c3028c6df713def8eda86c12a0141e2c5
Victor Vilar <victor.svilar@gmail.com>
2025-06-02 15:21:28 -0300

    feat:Criado campo para selecionar o tipo de export que tera o relatorio.

--- src/main/java/br/com/victorvilar/contaspagar/views/ProgramacaoPagamentoView.java
+++ src/main/java/br/com/victorvilar/contaspagar/views/ProgramacaoPagamentoView.java
@@ -26,9 +26,12 @@ public class ProgramacaoPagamentoView extends javax.swing.JFrame {
     private final ProgramacaoPagamentoController controller;
     
     public ProgramacaoPagamentoView(MovimentoPagamentoService service){
-        controller = new ProgramacaoPagamentoController(service, this);
-        initComponents();
-        setLocationRelativeTo(null);
+       controller = new ProgramacaoPagamentoController(service, this);
+       initComponents();
+       setLocationRelativeTo(null);
+       buttonGroup1.add(radioPdf);
+       buttonGroup1.add(radioCsv);
+       radioPdf.setSelected(true);
     }
     
     public JTextField getFieldDataInicial(){
@@ -48,6 +51,7 @@ public class ProgramacaoPagamentoView extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonGroup1 = new javax.swing.ButtonGroup();
         jButton1 = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
@@ -55,13 +59,17 @@ public class ProgramacaoPagamentoView extends javax.swing.JFrame {
         fieldDataInicial = new javax.swing.JTextField();
         fieldDataFinal = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        radioPdf = new javax.swing.JRadioButton();
+        radioCsv = new javax.swing.JRadioButton();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Gerar Programação");
-        setMaximumSize(new java.awt.Dimension(247, 176));
-        setMinimumSize(new java.awt.Dimension(247, 176));
-        setPreferredSize(new java.awt.Dimension(247, 176));
-        setSize(new java.awt.Dimension(247, 176));
+        setMaximumSize(new java.awt.Dimension(247, 228));
+        setMinimumSize(new java.awt.Dimension(247, 228));
+        setPreferredSize(new java.awt.Dimension(247, 228));
+        setSize(new java.awt.Dimension(247, 228));
 
         jButton1.setText("Gerar");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -71,7 +79,7 @@ public class ProgramacaoPagamentoView extends javax.swing.JFrame {
         });
 
         jLabel3.setFont(new java.awt.Font("Noto Sans", 1, 12)); // NOI18N
-        jLabel3.setText("Período");
+        jLabel3.setText("Período:");
 
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
 
@@ -120,6 +128,36 @@ public class ProgramacaoPagamentoView extends javax.swing.JFrame {
                 .addContainerGap(9, Short.MAX_VALUE))
         );
 
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
+
+        radioPdf.setText("PDF");
+
+        radioCsv.setText("CSV");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(radioPdf)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(radioCsv)
+                .addContainerGap(106, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(radioCsv)
+                    .addComponent(radioPdf))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jLabel4.setFont(new java.awt.Font("Noto Sans", 1, 12)); // NOI18N
+        jLabel4.setText("Exportar para:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -129,6 +167,8 @@ public class ProgramacaoPagamentoView extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel3)
                     .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)))
         );
         layout.setVerticalGroup(
@@ -139,6 +179,10 @@ public class ProgramacaoPagamentoView extends javax.swing.JFrame {
                 .addGap(3, 3, 3)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(7, 7, 7)
+                .addComponent(jLabel4)
+                .addGap(3, 3, 3)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(12, 12, 12)
                 .addComponent(jButton1))
         );
 
@@ -172,12 +216,17 @@ public class ProgramacaoPagamentoView extends javax.swing.JFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JTextField fieldDataFinal;
     private javax.swing.JTextField fieldDataInicial;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JRadioButton radioCsv;
+    private javax.swing.JRadioButton radioPdf;
     // End of variables declaration//GEN-END:variables
 }
