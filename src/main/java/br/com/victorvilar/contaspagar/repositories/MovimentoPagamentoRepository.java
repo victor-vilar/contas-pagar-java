40747f78bf4a4cf630a75c5c86bb077923242e88
Victor Vilar <victor.svilar@gmail.com>
2025-06-03 18:14:48 -0300

    feat:Criado metodo para buscar um movimento por sua integridade.

--- src/main/java/br/com/victorvilar/contaspagar/repositories/MovimentoPagamentoRepository.java
+++ src/main/java/br/com/victorvilar/contaspagar/repositories/MovimentoPagamentoRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.victorvilar.contaspagar.entities.MovimentoPagamento;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.Query;
 
 public interface MovimentoPagamentoRepository extends JpaRepository<MovimentoPagamento, Long> {
@@ -19,4 +21,6 @@ public interface MovimentoPagamentoRepository extends JpaRepository<MovimentoPag
     
     @Query("SELECT m FROM MovimentoPagamento m WHERE LOWER(m.despesa.nome) LIKE LOWER(CONCAT('%', :despesaName, '%')) AND dataVencimento >= :dataInicio AND dataVencimento <= :dataFim AND dataPagamento IS NOT NULL ORDER BY dataVencimento ASC")
     List<MovimentoPagamento> getBetweenDatesAndDespesaNamePago(LocalDate dataInicio, LocalDate dataFim, String despesaName);
+
+    Optional<MovimentoPagamento> findByIntegridade(String integridade);
 }
