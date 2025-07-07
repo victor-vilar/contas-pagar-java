79658a78f4d8e5a4e51bf74d98ad786c2492e1a8
Victor Vilar <victor.svilar@gmail.com>
2025-07-07 18:31:53 -0300

    feat:Criado migration para a tabela enderecos_fornecedor.

---
+++ src/main/resources/db/migration/V10__create_table_enderecos_fornecedor.sql
@@ -0,0 +1,11 @@
+create table enderecos_fornecedor(
+    id BIGSERIAL PRIMARY_KEY,
+    logradouro VARCHAR(150) NOT NULL,
+    numero VARCHAR(30) NOT NULL,
+    bairro VARCHAR(150)NOT NULL,
+    cidade VARCHAR(150) NOT NULL,
+    cep VARCHAR(8),
+    ponto_referencia(50),
+    fornecedor_id BIGINT,
+    CONSTRAINT fornecedor_fk FOREIGN_KEY(fornecedor_id)REFERENCES fornecedor(id)
+);
