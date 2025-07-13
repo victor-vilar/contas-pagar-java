0e0e1e945997fd508a15d908f166156a6eaa1ce8
Victor Vilar <victor.svilar@gmail.com>
2025-07-13 11:43:06 -0300

    feat:Criada interface FornecedorService.

---
+++ src/main/java/br/com/victorvilar/contaspagar/services/interfaces/FornecedorService.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.victorvilar.contaspagar.services.interfaces;
+
+import br.com.victorvilar.contaspagar.entities.Fornecedor;
+
+/**
+ *
+ * @author victor
+ */
+public interface FornecedorService extends AppService<Fornecedor>{
+    
+}
