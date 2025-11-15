c130a0a2b23cf1d6cf4b8dcc81cd1d55c1e37e86
Victor Vilar <victor.svilar@gmail.com>
2025-11-15 15:46:10 -0300

    test:criado testes parar os metodos de criar fornecedor e criar endereco.

--- src/test/java/br/com/victorvilar/contaspagar/controllers/FornecedorViewControllerTest.java
+++ src/test/java/br/com/victorvilar/contaspagar/controllers/FornecedorViewControllerTest.java
@@ -4,6 +4,8 @@
  */
 package br.com.victorvilar.contaspagar.controllers;
 
+import br.com.victorvilar.contaspagar.entities.EnderecoFornecedor;
+import br.com.victorvilar.contaspagar.entities.Fornecedor;
 import br.com.victorvilar.contaspagar.views.FornecedorView;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
@@ -42,11 +44,46 @@ public class FornecedorViewControllerTest {
     
     @Test
     public void metodoDeveCriarUmNovoEnderecoComInformacoesDaView(){
-       
+        view.getFieldBairro().setText("teste");
+        view.getFieldCep().setText("teste");
+        view.getFieldCidade().setText("teste");
+        view.getFieldLogradouro().setText("teste");
+        view.getFieldNumero().setText("teste");
+        view.getComboUF().setSelectedIndex(1);
+        EnderecoFornecedor endereco = controller.criarEndereco();
+        assertEquals(endereco.getBairro(), view.getFieldBairro().getText());
+        assertEquals(endereco.getCidade(), view.getFieldBairro().getText());
+        assertEquals(endereco.getCep(), view.getFieldBairro().getText());
+        assertEquals(endereco.getLogradouro(), view.getFieldLogradouro().getText());
+        assertEquals(endereco.getNumero(), view.getFieldNumero().getText());
+        assertEquals(endereco.getUf().toString(),(String) view.getComboUF().getSelectedItem());
     }
     
     @Test
     public void metodoDeveCriarUmNovoFornecedorComInformacoesDaView(){
+        view.getFieldBairro().setText("teste");
+        view.getFieldCep().setText("teste");
+        view.getFieldCidade().setText("teste");
+        view.getFieldCpfCnpj().setText("teste");
+        view.getFieldFantasia().setText("teste");
+        view.getFieldId().setText("teste");
+        view.getFieldLogradouro().setText("teste");
+        view.getFieldNome().setText("teste");
+        view.getFieldNumero().setText("teste");
+        view.getFieldObservacao().setText("teste");
+        view.getComboUF().setSelectedIndex(1);
+        
+        Fornecedor fornecedor = controller.criarFornecedor();
+        assertEquals(fornecedor.getCpfCnpj(), view.getFieldCpfCnpj().getText());
+        assertEquals(fornecedor.getRazaoSocial(), view.getFieldNome().getText());
+        assertEquals(fornecedor.getNomeFantasia(), view.getFieldFantasia().getText());
+        assertEquals(fornecedor.getObservacao(), view.getFieldObservacao().getText());
+        assertEquals(fornecedor.getEndereco().getBairro(), view.getFieldBairro().getText());
+        assertEquals(fornecedor.getEndereco().getCidade(), view.getFieldBairro().getText());
+        assertEquals(fornecedor.getEndereco().getCep(), view.getFieldBairro().getText());
+        assertEquals(fornecedor.getEndereco().getLogradouro(), view.getFieldLogradouro().getText());
+        assertEquals(fornecedor.getEndereco().getNumero(), view.getFieldNumero().getText());
+        assertEquals(fornecedor.getEndereco().getUf().toString(),(String) view.getComboUF().getSelectedItem());
     }
     
     @Test
@@ -54,7 +91,7 @@ public class FornecedorViewControllerTest {
         view.getFieldBairro().setText("teste");
         view.getFieldCep().setText("teste");
         view.getFieldCidade().setText("teste");
-        view.getFieldCnpjCpf().setText("teste");
+        view.getFieldCpfCnpj().setText("teste");
         view.getFieldFantasia().setText("teste");
         view.getFieldId().setText("teste");
         view.getFieldLogradouro().setText("teste");
@@ -67,7 +104,7 @@ public class FornecedorViewControllerTest {
         assertEquals(view.getFieldBairro().getText(),"");
         assertEquals(view.getFieldCep().getText(),"");
         assertEquals(view.getFieldCidade().getText(),"");
-        assertEquals(view.getFieldCnpjCpf().getText(),"");
+        assertEquals(view.getFieldCpfCnpj().getText(),"");
         assertEquals(view.getFieldFantasia().getText(),"");
         assertEquals(view.getFieldId().getText(),"");
         assertEquals(view.getFieldLogradouro().getText(),"");
