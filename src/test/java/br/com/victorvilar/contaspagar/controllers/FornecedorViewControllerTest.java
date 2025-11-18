8e9841a9c5ecdec628b6cd0efba40c46a606064e
Victor Vilar <victor.svilar@gmail.com>
2025-11-18 16:30:04 -0300

    test:Adicionando mais testes para FornecedorViewController.

--- src/test/java/br/com/victorvilar/contaspagar/controllers/FornecedorViewControllerTest.java
+++ src/test/java/br/com/victorvilar/contaspagar/controllers/FornecedorViewControllerTest.java
@@ -6,12 +6,18 @@ package br.com.victorvilar.contaspagar.controllers;
 
 import br.com.victorvilar.contaspagar.entities.EnderecoFornecedor;
 import br.com.victorvilar.contaspagar.entities.Fornecedor;
+import br.com.victorvilar.contaspagar.exceptions.FieldsEmBrancoException;
+import br.com.victorvilar.contaspagar.exceptions.QuantidadeDeParcelasException;
 import br.com.victorvilar.contaspagar.services.interfaces.FornecedorService;
 import br.com.victorvilar.contaspagar.views.FornecedorView;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.ArgumentMatchers.any;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.times;
@@ -39,26 +45,97 @@ public class FornecedorViewControllerTest {
         this.view = new FornecedorView(controller);
         view.inicializarFormulario();
     }
+    
+    @Test
+    @DisplayName("metodo salvar")
+    public void metodoSalvarDeveSalvarFornecedorComSucesso(){
+        view.getFieldBairro().setText("teste");
+        view.getFieldCep().setText("teste");
+        view.getFieldCidade().setText("teste");
+        view.getFieldCpfCnpj().setText("teste");
+        view.getFieldFantasia().setText("teste");
+        view.getFieldLogradouro().setText("teste");
+        view.getFieldNome().setText("teste");
+        view.getFieldNumero().setText("teste");
+        view.getFieldObservacao().setText("teste");
+        view.getComboUF().setSelectedIndex(1);
+        controller.salvar();
+        verify(service,times(1)).save(any(Fornecedor.class));
+    }
+    
+    @Test
+    @DisplayName("metodo salvar")
+    public void metodoSalvarDeveSalvarDeveLançarExceptionQuandoExistiremCamposEmBranco(){
+        Assertions.assertThrows(FieldsEmBrancoException.class, ()
+                -> controller.salvar());
+    }
+    
+    @Test
+    @DisplayName("metodo salvar")
+    public void deveChamarOsMetodosParaLimparEAtivarEDesativarCampos(){
+        view.getFieldBairro().setText("teste");
+        view.getFieldCep().setText("teste");
+        view.getFieldCidade().setText("teste");
+        view.getFieldCpfCnpj().setText("teste");
+        view.getFieldFantasia().setText("teste");
+        view.getFieldLogradouro().setText("teste");
+        view.getFieldNome().setText("teste");
+        view.getFieldNumero().setText("teste");
+        view.getFieldObservacao().setText("teste");
+        view.getComboUF().setSelectedIndex(1);
+        controller.salvar();
+        verify(controller,times(1)).limparCampos();
+        verify(controller,times(1)).ativarOuDesativarCampos(false);
+    }
    
     @Test
+    @DisplayName("metodo ativarOuDesativarCampos")
     public void metodoDeveAtivarTodosOsCamposDaViewQueNaoEstaoNaLista(){
-        
-    }
-    
-    @Test void metodoDeveDesativarTodosOsCamposDaView(){
-        
+        view.getAllComponents().stream().forEach(c -> c.setEnabled(false));
+        controller.ativarOuDesativarCampos(true);
+        List<String> excluded = controller.getExcludedComponents();
+        view.getAllComponents().stream().forEach(c -> {
+                if(!excluded.contains(c.getName())){
+                    assertEquals(c.isEnabled(),true);
+                }
+            }
+        );
     }
     
     @Test
-    void metodoDeletarDeveChamarMetodoDoServiceComOValorDaView(){
+    @DisplayName("metodo ativarOuDesativarCampos")
+    public void metodoDeveDesativarTodosOsCamposDaView(){
+        view.getAllComponents().stream().forEach(c -> c.setEnabled(true));
+        controller.ativarOuDesativarCampos(false);
+        List<String> excluded = controller.getExcludedComponents();
+        view.getAllComponents().stream().forEach(c -> {
+                if(!excluded.contains(c.getName())){
+                    assertEquals(c.isEnabled(),false);
+                }
+            }
+        );
+    }
+    
+    @Test
+            @DisplayName("metodo deletar")
+    void metodoDeletarDeveChamarMetodoDoServicePassandoOValorDaView(){
       view.getFieldId().setText("1"); 
       controller.deletar();
       verify(service,times(1)).deleteById(1l);
+    }
+    
+    @Test
+            @DisplayName("metodo deletar")
+    void metodoDeletarDeveChamarOMetodoLimparCamposEAtivarOuDesativarCampos(){
+      view.getFieldId().setText("1"); 
+      controller.deletar();
       verify(controller,times(1)).limparCampos();
       verify(controller,times(1)).ativarOuDesativarCampos(false);
     }
     
+    
     @Test
+    @DisplayName("metodo criarNovoEndereco")
     public void metodoDeveCriarUmNovoEnderecoComInformacoesDaView(){
         view.getFieldBairro().setText("teste");
         view.getFieldCep().setText("teste");
