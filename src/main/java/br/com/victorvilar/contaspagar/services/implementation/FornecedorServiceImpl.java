ce2d70ec219fea1f39d9faa8c33133ad97921e91
Victor Vilar <victor.svilar@gmail.com>
2025-11-19 14:20:21 -0300

    feat:Criado metodo para excluir fornecedores.

--- src/main/java/br/com/victorvilar/contaspagar/services/implementation/FornecedorServiceImpl.java
+++ src/main/java/br/com/victorvilar/contaspagar/services/implementation/FornecedorServiceImpl.java
@@ -71,7 +71,7 @@ public class FornecedorServiceImpl implements FornecedorService{
 
     @Override
     public void deleteById(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        repository.deleteById(id);
     }
 
     @Override
