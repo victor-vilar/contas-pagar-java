7b7386055d919cd21f691b2f77cd9e9500966064
Victor Vilar <victor.svilar@gmail.com>
2025-11-19 14:15:36 -0300

    feat:Criado metodos para atualizar fornecedor e o seu endereco.

--- src/main/java/br/com/victorvilar/contaspagar/services/implementation/FornecedorServiceImpl.java
+++ src/main/java/br/com/victorvilar/contaspagar/services/implementation/FornecedorServiceImpl.java
@@ -4,7 +4,9 @@
  */
 package br.com.victorvilar.contaspagar.services.implementation;
 
+import br.com.victorvilar.contaspagar.entities.EnderecoFornecedor;
 import br.com.victorvilar.contaspagar.entities.Fornecedor;
+import br.com.victorvilar.contaspagar.exceptions.FornecedorNotFoundException;
 import br.com.victorvilar.contaspagar.repositories.FornecedorRepository;
 import br.com.victorvilar.contaspagar.services.interfaces.FornecedorService;
 import java.util.List;
@@ -47,8 +49,25 @@ public class FornecedorServiceImpl implements FornecedorService{
 
     @Override
     public Fornecedor update(Fornecedor obj) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Fornecedor fornecedorParaAtualizar = getById(obj.getId());
+        fornecedorParaAtualizar.setCpfCnpj(obj.getCpfCnpj());
+        fornecedorParaAtualizar.setRazaoSocial(obj.getRazaoSocial());
+        fornecedorParaAtualizar.setNomeFantasia(obj.getNomeFantasia());
+        fornecedorParaAtualizar.setObservacao(obj.getObservacao());
+        atualizarEndereco(fornecedorParaAtualizar.getEndereco(),obj.getEndereco());
+        return save(fornecedorParaAtualizar);
     }
+    
+    public void atualizarEndereco(EnderecoFornecedor end1, EnderecoFornecedor end2){
+        end1.setBairro(end2.getBairro());
+        end1.setCep(end2.getCep());
+        end1.setCidade(end2.getCidade());
+        end1.setLogradouro(end2.getLogradouro());
+        end1.setNumero(end2.getNumero());
+        end1.setObservacao(end2.getObservacao());
+        end1.setUf(end2.getUf());
+    }
+    
 
     @Override
     public void deleteById(Long id) {
